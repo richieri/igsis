5d293a4d9d5330c622e3eadc5b60ffab020360e4
Amanda Crivelaro <amd.crivelaro@gmail.com>
2017-01-18 11:15:12 -0200

    limpeza modulo contabilidade

--- perfil/m_contabilidade/frm_lista_contabilpf.php
+++ perfil/m_contabilidade/frm_lista_contabilpf.php
@@ -1,10 +1,6 @@
 <?php
-
-$linha_tabela_lista = siscontratLista(1,"",1000,1,"DESC",8); //esse gera uma array com os pedidos
-//$tipoPessoa,$instituicao,$num_registro,$pagina,$ordem,$estado
-
-$link="index.php?perfil=contabilidade&p=frm_cadastra_contabilpf&id_ped=";
-
+	$linha_tabela_lista = siscontratLista(1,"",1000,1,"DESC",8); //esse gera uma array com os pedidos
+	$link="index.php?perfil=contabilidade&p=frm_cadastra_contabilpf&id_ped=";
 ?>
 	
 <?php include 'includes/menu.php';?>	
@@ -12,39 +8,37 @@ $link="index.php?perfil=contabilidade&p=frm_cadastra_contabilpf&id_ped=";
 	 <!-- inicio_list -->
 	<section id="list_items">
 		<div class="container">
-			 <div class="form-group"><br><br><h5><b>PESSOA FÍSICA</h5></b></div>
-			<div class="table-responsive list_info">
-				<table class="table table-condensed"><script type=text/javascript language=JavaScript src=../js/find2.js> </script>
+			<div class="form-group"><br><br><h5><b>PESSOA FÍSICA</h5></b></div>
+				<div class="table-responsive list_info">
+					<table class="table table-condensed"><script type=text/javascript language=JavaScript src=../js/find2.js> </script>
 					<thead>
-						<tr class="list_menu">
-							<td>Codigo do Pedido</td>
-							<td>Processo</td>
-							<td>Proponente</td>
-							<td>Objeto</td>
-							<td>Local</td>
-							<td>Periodo</td>
-						</tr>
+					<tr class="list_menu">
+					<td>Codigo do Pedido</td>
+					<td>Processo</td>
+					<td>Proponente</td>
+					<td>Objeto</td>
+					<td>Local</td>
+					<td>Periodo</td>
+					</tr>
 					</thead>
 					<tbody>
 <?php
-$data=date('Y');
-for($i = 0; $i < count($linha_tabela_lista); $i++)
- {
-	$linha_tabela_pedido_contratacaopf = recuperaDados("sis_pessoa_fisica",$linha_tabela_lista[$i]['IdProponente'],"Id_PessoaFisica");	 
-	echo "<tr><td class='lista'> <a href='".$link.$linha_tabela_lista[$i]['idPedido']."'>".$linha_tabela_lista[$i]['idPedido']."</a></td>";
-	echo '<td class="list_description">'.$linha_tabela_lista[$i]['NumeroProcesso'].'</td>';
-	echo '<td class="list_description">'.$linha_tabela_pedido_contratacaopf['Nome'].'</td> ';
-	echo '<td class="list_description">'.$linha_tabela_lista[$i]['Objeto'].'</td> ';
-	echo '<td class="list_description">'.$linha_tabela_lista[$i]['Local'].'</td> ';
-	echo '<td class="list_description">'.$linha_tabela_lista[$i]['Periodo'].'</td> </tr>';
-	}
+	$data=date('Y');
+	for($i = 0; $i < count($linha_tabela_lista); $i++)
+{
+		$linha_tabela_pedido_contratacaopf = recuperaDados("sis_pessoa_fisica",$linha_tabela_lista[$i]['IdProponente'],"Id_PessoaFisica");	 
+			echo "<tr><td class='lista'> <a href='".$link.$linha_tabela_lista[$i]['idPedido']."'>".$linha_tabela_lista[$i]['idPedido']."</a></td>";
+			echo '<td class="list_description">'.$linha_tabela_lista[$i]['NumeroProcesso'].'</td>';
+			echo '<td class="list_description">'.$linha_tabela_pedido_contratacaopf['Nome'].'</td> ';
+			echo '<td class="list_description">'.$linha_tabela_lista[$i]['Objeto'].'</td> ';
+			echo '<td class="list_description">'.$linha_tabela_lista[$i]['Local'].'</td> ';
+			echo '<td class="list_description">'.$linha_tabela_lista[$i]['Periodo'].'</td> </tr>';
+}
 
-?>
-	
-					
+?>					
 					</tbody>
-				</table>
-			</div>
+					</table>
+				</div>
 		</div>
 	</section>
 
