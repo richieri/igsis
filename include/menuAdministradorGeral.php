45dff578ddc6ddbe63d892fbc7487d3f763ea507
William Cordeiro Fonseca <x355804@rede.sp>
2017-01-18 13:18:31 -0200

    administradores

---
+++ include/menuAdministradorGeral.php
@@ -0,0 +1,27 @@
+<div class="menu-area">
+	<div id="dl-menu" class="dl-menuwrapper">
+		<button class="dl-trigger">Open Menu</button>
+		<ul class="dl-menu">
+			<li><a href="?perfil=admin&p=visaogeral">Usuários ativos</a></li>
+   			<li><a href="?perfil=admin&p=estatistica"> Estatísticas</a></li>
+			<li><a href="?perfil=admin&p=reabertura"> Reabrir eventos enviados</a></li>
+   			<li><a href="?perfil=admin&p=scripts"> Scripts</a>
+				<ul class="dl-submenu">
+					<li><a href="?perfil=admin&p=scripts&atualizar=agenda">Atualizar agenda</a></li>
+					<li><a href="?perfil=admin&p=scripts&status=1">Atualizar status</a></li>
+					<li><a href="?perfil=admin&p=scripts&inst_agenda=1">Atualizar Instituições/Agenda</a></li>
+					<li><a href="?perfil=admin&p=scripts&limpar_base=1">Limpar base de eventos</a></li>
+					<li><a href="?perfil=admin&p=scripts&empenho=1">Atualizar status N.E.</a></li>
+					<li><a href="?perfil=admin&p=scripts&formacao=1">Importar Base PF Formação</a></li>
+					<li><a href="?perfil=admin&p=scripts&contabilidade=1">Verificar tabela contabilidade</a></li>
+				</ul>
+			</li>
+			<li><a href="?perfil=admin&p=email">E-mail para solicitação de reenvio</a></li>
+   			<li><a href="?perfil=admin&p=contratos">Contratos</a></li>
+			<li><a href="?perfil=admin">Menu Principal</a></li>
+			<li><a href="?secao=perfil">Carregar Módulos</a></li>
+			<li><a href="?secao=ajuda">Ajuda</a></li>
+			<li><a href="../include/logoff.php">Sair </a></li>
+		</ul>
+	</div><!-- /dl-menuwrapper -->
+</div>
---
+++ include/menuAdministradorLocal.php
@@ -0,0 +1,16 @@
+<div class="menu-area">
+	<div id="dl-menu" class="dl-menuwrapper">
+		<button class="dl-trigger">Open Menu</button>
+		<ul class="dl-menu">
+			<li><a href="?perfil=administrador&p=users"> Listar Usuários</a></li>
+			<li><a href="?perfil=administrador&p=espacos"> Listar Espaços</a></li>
+			<li><a href="?perfil=administrador&p=listaprojetoespecial"> Listar Projetos Especiais</a></li>
+			<li><a href="?perfil=administrador&p=eventos"> Listar Eventos</a></li>
+			<li><a href="?perfil=administrador&p=alteracoes"> Alterações</a></li>
+			<li><a href="?perfil=admin">Menu Principal</a></li>
+			<li><a href="?secao=perfil">Carregar Módulos</a></li>
+			<li><a href="http://www.centrocultural.cc/igsis/manual/">Ajuda</a></li>
+			<li><a href="../include/logoff.php">Sair</a></li>				
+		</ul>
+	</div><!-- /dl-menuwrapper !-->
+</div>
