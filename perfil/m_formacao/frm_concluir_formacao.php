c2625e4d8c2bb8f8addb4647e90ceb40a9caa5a9
will <willzenhu@gmail.com>
2017-01-31 13:03:44 -0200

    formacao

--- perfil/m_formacao/frm_concluir_formacao.php
+++ perfil/m_formacao/frm_concluir_formacao.php
@@ -1,24 +1,24 @@
-﻿<?php include 'includes/menu.php';?>
-
-<?php
-if(isset($_GET['b'])){
-	$b = $_GET['b'];	
-}else{
-	$b = 'inicial';
-}
-
-switch($b)
-{
-	case 'inicial':
-	if(isset($_POST['pesquisar']))
+﻿<?php
+	include 'includes/menu.php';
+	if(isset($_GET['b']))
 	{
-		$id = trim($_POST['id']);
-		$estado = $_POST['estado'];
-		$processo = $_POST['NumeroProcesso'];
-
-		if($id == "" AND $estado == 0 AND $processo == 0 )
-		{ ?>
-	
+		$b = $_GET['b'];	
+	}
+	else
+	{
+		$b = 'inicial';
+	}
+	switch($b)
+	{
+		case 'inicial':
+			if(isset($_POST['pesquisar']))
+			{
+				$id = trim($_POST['id']);
+				$estado = $_POST['estado'];
+				$processo = $_POST['NumeroProcesso'];
+				if($id == "" AND $estado == 0 AND $processo == 0 )
+				{
+?>	
 <section id="services" class="home-section bg-white">
 	<div class="container">
 		<div class="row">
@@ -29,238 +29,228 @@ switch($b)
 				</div>
 			</div>
 		</div>
-		
-	<div class="row">
-		<div class="form-group">
-			<div class="col-md-offset-2 col-md-8">
-				<h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
-				<form method="POST" action="?perfil=formacao&p=frm_concluir_formacao" class="form-horizontal" role="form">
-					<label>Código do Pedido</label>
-					<input type="text" name="id" class="form-control" id="palavras" placeholder="Insira o Código do Pedido" ><br />
-						<label>Número do Processo</label>
-						<input type="text" name="NumeroProcesso" class="form-control" id="palavras" placeholder="Insira o número do processo com a devida pontuação"><br />           			          
+		<div class="row">
+			<div class="form-group">
+				<div class="col-md-offset-2 col-md-8">
+					<h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
+						<form method="POST" action="?perfil=formacao&p=frm_concluir_formacao" class="form-horizontal" role="form">
+							<label>Código do Pedido</label>
+							<input type="text" name="id" class="form-control" id="palavras" placeholder="Insira o Código do Pedido" ><br />
+							<label>Número do Processo</label>
+							<input type="text" name="NumeroProcesso" class="form-control" id="palavras" placeholder="Insira o número do processo com a devida pontuação"><br />           			          
 							<label>Status do pedido</label>
-								<select class="form-control" name="estado" id="inputSubject" >
-									<option value='0'></option>
-									<?php echo geraOpcao("sis_estado","","") ?>
-								</select>	
+							<select class="form-control" name="estado" id="inputSubject" >
+								<option value='0'></option>
+								<?php echo geraOpcao("sis_estado","","") ?>
+							</select>
+							<br />
+							<div class="form-group">
+								<div class="col-md-offset-2 col-md-8">
+									<input type="hidden" name="pesquisar" value="1" />
+									<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
+								</div>
+							</div>
+						</form>
+				</div>
 			</div>
 		</div>
-<br />    
-         
-	<div class="form-group">
-		<div class="col-md-offset-2 col-md-8">
-			<input type="hidden" name="pesquisar" value="1" />
-			<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
-			</form>
-		</div>
-	</div>
 	</div>
 </section>
-
-<?php
-	}
-	else
-	{
-		$con = bancoMysqli();
-		$sql_existe = "SELECT * FROM igsis_pedido_contratacao, sis_formacao WHERE sis_formacao.idPedidoContratacao = igsis_pedido_contratacao.idPedidoContratacao AND igsis_pedido_contratacao.publicado = '1' AND tipoPessoa = '4' AND estado IS NOT NULL ORDER BY igsis_pedido_contratacao.idPedidoContratacao DESC";
-		$query_existe = mysqli_query($con, $sql_existe);
-		$num_registro = mysqli_num_rows($query_existe);
-		if($id != "" AND $num_registro > 0) // Foi inserido o número do pedido
-		{
-			$pedido = recuperaDados("igsis_pedido_contratacao",$id,"idPedidoContratacao");
-			if($pedido['estado'] != NULL)
-			{
-				$pessoa = recuperaPessoa($pedido['idPessoa'],4);
-				if($pedido['parcelas'] > 1)
-				{
-					$valorTotal = somaParcela($pedido['idPedidoContratacao'],$pedido['parcelas']);
-					$formaPagamento = txtParcelas($pedido['idPedidoContratacao'],$pedido['parcelas']);	
+			<?php
 				}
 				else
 				{
-					$valorTotal = $pedido['valor'];
-					$formaPagamento = $pedido['formaPagamento'];
-				}
-				$x[0]['id']= $pedido['idPedidoContratacao'];
-				$x[0]['NumeroProcesso'] = $pedido['NumeroProcesso'];
-				$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
-				$x[0]['proponente'] = $pessoa['Nome'];
-				$x[0]['status'] = $pedido['estado'];
-				$x['num'] = 1;
-			}
-			else
-			{
-				$x['num'] = 0;		
-			}
-		}
-		else
-		{ //Não foi inserido o número do pedido
-		
-			if($estado == 0)
-			{
-				$filtro_status = " AND estado IS NOT NULL ";	
-			}
-			else
-			{
-				$filtro_status = " AND igsis_pedido_contratacao.estado = '$estado'  ";	
-			}	
-			if($processo == 0)
-			{
-				$filtro_processo = "";	
-			}
-			else
-			{
-				$filtro_processo = " AND igsis_pedido_contratacao.NumeroProcesso LIKE '%$processo%'  ";	
-			}
-			$sql_evento = "SELECT * FROM sis_formacao,igsis_pedido_contratacao WHERE sis_formacao.publicado = '1' AND igsis_pedido_contratacao.publicado = '1' AND sis_formacao.idPedidoContratacao = igsis_pedido_contratacao.idPedidoContratacao $filtro_processo $filtro_status ORDER BY igsis_pedido_contratacao.idPedidoContratacao DESC";
-			$query_evento = mysqli_query($con,$sql_evento);
-			$i = 0;
-			while($evento = mysqli_fetch_array($query_evento))
-			{
-				$sql_existe = "SELECT sis_formacao.idPedidoContratacao FROM igsis_pedido_contratacao, sis_formacao WHERE sis_formacao.idPedidoContratacao = igsis_pedido_contratacao.idPedidoContratacao AND igsis_pedido_contratacao.publicado = '1' $filtro_status ";
-				//$query_existe = mysqli_query($con, $sql_existe);
-				if(mysqli_num_rows($query_evento) > 0)
-				{
-					$pedido = recuperaDados("igsis_pedido_contratacao",$evento['idPedidoContratacao'],"idPedidoContratacao");
-					$pessoa = recuperaPessoa($pedido['idPessoa'],4);
-					if($pedido['parcelas'] > 1)
+					$con = bancoMysqli();
+					$sql_existe = "SELECT * FROM igsis_pedido_contratacao, sis_formacao WHERE sis_formacao.idPedidoContratacao = igsis_pedido_contratacao.idPedidoContratacao AND igsis_pedido_contratacao.publicado = '1' AND tipoPessoa = '4' AND estado IS NOT NULL ORDER BY igsis_pedido_contratacao.idPedidoContratacao DESC";
+					$query_existe = mysqli_query($con, $sql_existe);
+					$num_registro = mysqli_num_rows($query_existe);
+					if($id != "" AND $num_registro > 0) // Foi inserido o número do pedido
 					{
-						$valorTotal = somaParcela($pedido['idPedidoContratacao'],$pedido['parcelas']);
-						$formaPagamento = txtParcelas($pedido['idPedidoContratacao'],$pedido['parcelas']);	
+						$pedido = recuperaDados("igsis_pedido_contratacao",$id,"idPedidoContratacao");
+						if($pedido['estado'] != NULL)
+						{
+							$pessoa = recuperaPessoa($pedido['idPessoa'],4);
+							if($pedido['parcelas'] > 1)
+							{
+								$valorTotal = somaParcela($pedido['idPedidoContratacao'],$pedido['parcelas']);
+								$formaPagamento = txtParcelas($pedido['idPedidoContratacao'],$pedido['parcelas']);	
+							}
+							else
+							{
+								$valorTotal = $pedido['valor'];
+								$formaPagamento = $pedido['formaPagamento'];
+							}
+							$x[0]['id']= $pedido['idPedidoContratacao'];
+							$x[0]['NumeroProcesso'] = $pedido['NumeroProcesso'];
+							$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
+							$x[0]['proponente'] = $pessoa['Nome'];
+							$x[0]['status'] = $pedido['estado'];
+							$x['num'] = 1;
+						}
+						else
+						{
+							$x['num'] = 0;		
+						}
 					}
 					else
-					{
-						$valorTotal = $pedido['valor'];
-						$formaPagamento = $pedido['formaPagamento'];
+					{ //Não foi inserido o número do pedido
+						if($estado == 0)
+						{
+							$filtro_status = " AND estado IS NOT NULL ";	
+						}
+						else
+						{
+							$filtro_status = " AND igsis_pedido_contratacao.estado = '$estado'  ";	
+						}	
+						if($processo == 0)
+						{
+							$filtro_processo = "";	
+						}
+						else
+						{
+							$filtro_processo = " AND igsis_pedido_contratacao.NumeroProcesso LIKE '%$processo%'  ";	
+						}
+						$sql_evento = "SELECT * FROM sis_formacao,igsis_pedido_contratacao WHERE sis_formacao.publicado = '1' AND igsis_pedido_contratacao.publicado = '1' AND sis_formacao.idPedidoContratacao = igsis_pedido_contratacao.idPedidoContratacao $filtro_processo $filtro_status ORDER BY igsis_pedido_contratacao.idPedidoContratacao DESC";
+						$query_evento = mysqli_query($con,$sql_evento);
+						$i = 0;
+						while($evento = mysqli_fetch_array($query_evento))
+						{
+							$sql_existe = "SELECT sis_formacao.idPedidoContratacao FROM igsis_pedido_contratacao, sis_formacao WHERE sis_formacao.idPedidoContratacao = igsis_pedido_contratacao.idPedidoContratacao AND igsis_pedido_contratacao.publicado = '1' $filtro_status ";
+							//$query_existe = mysqli_query($con, $sql_existe);
+							if(mysqli_num_rows($query_evento) > 0)
+							{
+								$pedido = recuperaDados("igsis_pedido_contratacao",$evento['idPedidoContratacao'],"idPedidoContratacao");
+								$pessoa = recuperaPessoa($pedido['idPessoa'],4);
+								if($pedido['parcelas'] > 1)
+								{
+									$valorTotal = somaParcela($pedido['idPedidoContratacao'],$pedido['parcelas']);
+									$formaPagamento = txtParcelas($pedido['idPedidoContratacao'],$pedido['parcelas']);	
+								}
+								else
+								{
+									$valorTotal = $pedido['valor'];
+									$formaPagamento = $pedido['formaPagamento'];
+								}
+								$x[$i]['id']= $pedido['idPedidoContratacao'];
+								$x[$i]['NumeroProcesso']= $pedido['NumeroProcesso'];
+								$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
+								$x[$i]['proponente'] = $pessoa['Nome'];
+								$x[$i]['status'] = $pedido['estado'];	
+								$i++;
+							}
+						}
+						$x['num'] = $i;
 					}
-		
-						$x[$i]['id']= $pedido['idPedidoContratacao'];
-						$x[$i]['NumeroProcesso']= $pedido['NumeroProcesso'];
-						$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
-						$x[$i]['proponente'] = $pessoa['Nome'];
-						$x[$i]['status'] = $pedido['estado'];	
-						$i++;
 				}
-			}
-			$x['num'] = $i;
-		}
-	} 
-	$mensagem = "Foram encontradas ".$x['num']." pedido(s) de contratação.";
-	?>
-	
-	<br />
-	<br />
-	
+				$mensagem = "Foram encontradas ".$x['num']." pedido(s) de contratação.";
+			?>
+<br />
+<br />	
 <section id="list_items">
 	<div class="container">
-		 <h3>Resultado da busca</h3>
-		 <h5>Foram encontrados <?php if($pedido['tipoPessoa'] == 4)
-									 { 
-										echo $x['num'];
-									 }
-									 else
-									 {
-										echo  $x['num'] = 0; 
-									 }	 ?> pedidos de contratação.</h5>
-		 <h5><a href="?perfil=formacao&p=frm_concluir_formacao">Fazer outra busca</a></h5>
-			<div class="table-responsive list_info">
-				<?php 
+		<h3>Resultado da busca</h3>
+		<h5>Foram encontrados
+			<?php
+				if($pedido['tipoPessoa'] == 4)
+				{ 
+					echo $x['num'];
+				}
+				else
+				{
+					echo  $x['num'] = 0; 
+				}
+			?>
+			pedidos de contratação.
+		</h5>
+		<h5><a href="?perfil=formacao&p=frm_concluir_formacao">Fazer outra busca</a></h5>
+		<div class="table-responsive list_info">
+			<?php 
 				if($x['num'] == 0)
-				{ ?>
-				<?php }else{ ?>
-				
-				<table class="table table-condensed">
-					<thead>
-						<tr class="list_menu">
-							<td>Processo</td>
-								<td>Codigo do Pedido</td>
-									<td>Proponente</td>
-										<td>Status</td>
-						</tr>
-					</thead>
-					
-<tbody>
-
-<?php
-	$data=date('Y');
-	for($h = 0; $h < $x['num']; $h++)
-	{
-		if($pedido['tipoPessoa'] == 4)
-		{
-		//$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
-		$status = recuperaDados("sis_estado",$x[$h]['status'],"idEstado");
-		//$ped = siscontrat($pedido['idPedidoContratacao']);	 
-
-		echo "<tr><td class='lista'> <a href='?perfil=formacao&p=frm_concluir_final_formacao&id_ped=".$x[$h]['id']."'>".$x[$h]['NumeroProcesso']."</a></td>";
-	
-		echo '<td class="list_description">'.$x[$h]['id'].		  '</td>';
-		echo '<td class="list_description">'.$x[$h]['proponente'].'</td> ';
-		echo '<td class="list_description">'.$status['estado'].	  '</td> ';
-		}
-	}
-?>
-			
-</tbody>
-				</table>
-				
-<?php } ?>		
-			</div>	
+				{
+				}
+				else
+				{
+			?>	
+			<table class="table table-condensed">
+				<thead>
+					<tr class="list_menu">
+						<td>Processo</td>
+						<td>Codigo do Pedido</td>
+						<td>Proponente</td>
+						<td>Status</td>
+					</tr>
+				</thead>
+				<tbody>
+				<?php
+					$data=date('Y');
+					for($h = 0; $h < $x['num']; $h++)
+					{
+						if($pedido['tipoPessoa'] == 4)
+						{
+							//$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
+							$status = recuperaDados("sis_estado",$x[$h]['status'],"idEstado");
+							//$ped = siscontrat($pedido['idPedidoContratacao']);
+							echo "<tr><td class='lista'> <a href='?perfil=formacao&p=frm_concluir_final_formacao&id_ped=".$x[$h]['id']."'>".$x[$h]['NumeroProcesso']."</a></td>";	
+							echo '<td class="list_description">'.$x[$h]['id'].		  '</td>';
+							echo '<td class="list_description">'.$x[$h]['proponente'].'</td> ';
+							echo '<td class="list_description">'.$status['estado'].	  '</td> ';
+						}
+					}
+				?>		
+				</tbody>
+			</table>
+			<?php
+				}
+			?>		
+		</div>	
 	</div>
 </section>
-
-<?php
-	}
-	else
-	{
-?>
-
+		<?php
+			}
+			else
+			{
+		?>
 <section id="services" class="home-section bg-white">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-offset-2 col-md-8">
 				<div class="section-heading">
-				<h2>Busca Formação</h2></div>
+					<h2>Busca Formação</h2>
+				</div>
 			</div>
-		</div>
-				  
-	<div class="row">
-		<div class="form-group">
-			<div class="col-md-offset-2 col-md-8">
-				<h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
-					<form method="POST" action="?perfil=formacao&p=frm_concluir_formacao" class="form-horizontal" role="form">
-						<label>Código do Pedido</label>
-						<input type="text" name="id" class="form-control" id="palavras" placeholder="Insira o Código do Pedido" ><br />
+		</div>		  
+		<div class="row">
+			<div class="form-group">
+				<div class="col-md-offset-2 col-md-8">
+					<h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
+						<form method="POST" action="?perfil=formacao&p=frm_concluir_formacao" class="form-horizontal" role="form">
+							<label>Código do Pedido</label>
+							<input type="text" name="id" class="form-control" id="palavras" placeholder="Insira o Código do Pedido" ><br />
 							<label>Número do Processo</label>
 							<input type="text" name="NumeroProcesso" class="form-control" id="palavras" placeholder="Insira número do processo" ><br />
-								<label>Status do pedido</label>
-								<select class="form-control" name="estado" id="inputSubject" >
-									<option value=""></option>
-									<?php echo geraOpcao("sis_estado","","") ?>
-								</select><br />
-			</div>
-		</div>
-<br />             
-	<div class="form-group">
-		<div class="col-md-offset-2 col-md-8">
-			<input type="hidden" name="pesquisar" value="1" />
-			<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
-					</form>
+							<label>Status do pedido</label>
+							<select class="form-control" name="estado" id="inputSubject" >
+								<option value=""></option>
+								<?php echo geraOpcao("sis_estado","","") ?>
+							</select>
+							<br />
+							<br />             
+							<div class="form-group">
+								<div class="col-md-offset-2 col-md-8">
+									<input type="hidden" name="pesquisar" value="1" />
+									<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
+								</div>
+							</div>
+						</form>
+				</div>
+			</div>		
 		</div>
 	</div>
-	</div>
-</section>               
-
-<?php
-	} 
-?>
-
-<?php
-	break;
-	case 'periodo': //
-?>
-
-<?php
-	break;
-} // fim da switch
-?>
+</section>
+		<?php
+			}
+		break;
+		case 'periodo':
+		break;
+	} // fim da switch
+		?>
--- perfil/m_formacao/frm_lista_dadoscontratacao.php
+++ perfil/m_formacao/frm_lista_dadoscontratacao.php
@@ -1,27 +1,27 @@
 <?php 
-include 'includes/menu.php';
-
-$con = bancoMysqli();
-$ano = date('Y');
-
-$pasta = "?perfil=formacao&p=frm_lista_dadoscontratacao&pag=";
-
-if(isset($_GET['pag'])){
-	$p = $_GET['pag'];
-	}else{
-	$p = $ano;	
-}
+	include 'includes/menu.php';
+	$con = bancoMysqli();
+	$ano = date('Y');
+	$pasta = "?perfil=formacao&p=frm_lista_dadoscontratacao&pag=";
+	if(isset($_GET['pag']))
+	{
+		$p = $_GET['pag'];
+	}
+	else
+	{
+		$p = $ano;	
+	}
 ?>
-
 <section id="list_items">
 	<div class="container">
-		<div class="sub-title"><br/><br/><h4>Escolha o ano<br/> 
-			| <a href="<?php echo $pasta?><?php echo $ano;?>"><?php echo $ano; ?></a> 
-			| <a href="<?php echo $pasta?><?php echo $ano - 1; ?>"><?php echo $ano - 1; ?></a> |
-		</h4></div>
+		<div class="sub-title"><br/><br/>
+			<h4>Escolha o ano<br/> 
+				| <a href="<?php echo $pasta?><?php echo $ano;?>"><?php echo $ano; ?></a> 
+				| <a href="<?php echo $pasta?><?php echo $ano - 1; ?>"><?php echo $ano - 1; ?></a> |
+			</h4>
+		</div>
 	</div>
 </section>
-
 <section id="list_items">
 	<div class="container">
 		<div class="sub-title"><br/><h5>Dados de contratação</h5>
@@ -40,34 +40,33 @@ if(isset($_GET['pag'])){
 					</tr>
 				</thead>
 				<tbody>
-				<?php
-				switch($p)
-				{
-					case $ano:
-					$sql_lista_formacao = "SELECT * FROM sis_formacao WHERE publicado = '1' AND Ano = $ano";
-					break;
-
-					case $ano - 1:
-					$sql_lista_formacao = "SELECT * FROM sis_formacao WHERE publicado = '1' AND Ano = $ano - 1";
-					break; 
-				} 
-					$query_lista_formacao = mysqli_query($con,$sql_lista_formacao);
-					while($formacao = mysqli_fetch_array($query_lista_formacao))
-					{
-						$pessoa = recuperaDados("sis_pessoa_fisica",$formacao['IdPessoaFisica'],"Id_PessoaFisica");	
-				?>
-						<tr>
-							<td class="list_description"><?php echo $formacao['Id_Formacao'];  ?></td>
-							<td class="list_description"><a href="?perfil=formacao&p=frm_cadastra_dadoscontratacao&id=<?php echo $formacao['Id_Formacao']; ?>"><?php echo $pessoa['Nome'];  ?></a></td>
-							<td class="list_description"><?php echo $pessoa['Telefone1'];  ?></td>
-							<td class="list_description"><?php echo retornaPrograma($formacao['IdPrograma']);  ?></td>
-							<td class="list_description"><?php echo $formacao['Ano'];  ?></td>
-							<td class="list_description"><?php echo retornaCargo($formacao['IdCargo']);  ?></td>
-							<td class="list_description"><?php echo retornaStatus($formacao['Status']);  ?></td>    
-						</tr>
-				<?php 
-					}
-				?>
+<?php
+	switch($p)
+	{
+		case $ano:
+			$sql_lista_formacao = "SELECT * FROM sis_formacao WHERE publicado = '1' AND Ano = $ano";
+		break;
+		case $ano - 1:
+			$sql_lista_formacao = "SELECT * FROM sis_formacao WHERE publicado = '1' AND Ano = $ano - 1";
+		break; 
+	} 
+	$query_lista_formacao = mysqli_query($con,$sql_lista_formacao);
+	while($formacao = mysqli_fetch_array($query_lista_formacao))
+	{
+		$pessoa = recuperaDados("sis_pessoa_fisica",$formacao['IdPessoaFisica'],"Id_PessoaFisica");	
+?>
+					<tr>
+						<td class="list_description"><?php echo $formacao['Id_Formacao'];  ?></td>
+						<td class="list_description"><a href="?perfil=formacao&p=frm_cadastra_dadoscontratacao&id=<?php echo $formacao['Id_Formacao']; ?>"><?php echo $pessoa['Nome'];  ?></a></td>
+						<td class="list_description"><?php echo $pessoa['Telefone1'];  ?></td>
+						<td class="list_description"><?php echo retornaPrograma($formacao['IdPrograma']);  ?></td>
+						<td class="list_description"><?php echo $formacao['Ano'];  ?></td>
+						<td class="list_description"><?php echo retornaCargo($formacao['IdCargo']);  ?></td>
+						<td class="list_description"><?php echo retornaStatus($formacao['Status']);  ?></td>    
+					</tr>
+<?php 
+	}
+?>
 				</tbody>
 			</table>
 		</div>
