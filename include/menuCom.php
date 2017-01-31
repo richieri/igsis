8b3fb890049e58a3634acefc706a31e964e35e79
Grayce de Deus Ferreira <grayce.ferreira@gmail.com>
2017-01-31 13:48:38 -0200

    Menu Comunicação

--- include/menuCom.php
+++ include/menuCom.php
@@ -16,6 +16,7 @@
                                         <li> <a href="?perfil=comunicacao&p=foto&foto=0" >Pendentes</a></li>
                                         <li> <a href="?perfil=comunicacao&p=foto" >Todos os eventos</a>  </li>
                                     </ul>
+					<li><a href="?perfil=comunicacao&p=docs" >Gerar Docs</a></li>                
                          </li>   
                              
                     
