42714dcd76d90d3b6722ab5b11d90909ec257f11
Grayce de Deus Ferreira <grayce.ferreira@gmail.com>
2017-02-03 15:36:41 -0200

    Alterações e Correções (Até Pagamento)

--- perfil/finalizar.php
+++ perfil/finalizar.php
@@ -81,7 +81,7 @@
 							Este número é a referência para as áreas de Contratos, Jurídico, Finanças, Contabilidade entre outros.<br />
 							<strong><a target='_blank' href='?perfil=detalhes_contrato&id_ped=".$pedidos[$i]."'>Clique aqui caso queira visualizar os detalhes desta contratação.</a></strong>
 							<br /><br />
-							<a href='http://www.centrocultural.cc/igsis/manual/index.php/2016/01/24/numero-ig-e-numero-pedido-de-contratacao/' target='_blank'>Saiba mais sobre os números gerados no nosso <i>Manual do Sistema</i></a>.<br /><br /><br />
+							<a href='http://www.centrocultural.cc/igsis/manual/index.php/introducao-ao-sistema-igsis/numero-igpedido-de-contratacao/' target='_blank'>Saiba mais sobre os números gerados no nosso <i>Manual do Sistema</i></a>.<br /><br /><br />
 							";
 							$i++;
 						}
