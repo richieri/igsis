c51677504014fcdce7cb7bff4b8f73b8d0f57ca6
LoreleiGab <loreleigab@hotmail.com>
2017-03-01 18:29:23 -0300

    Inclusão do novo formato de parecer artístico - Parte 01

--- perfil/contratados.php
+++ perfil/contratados.php
@@ -1461,10 +1461,10 @@
 		?>
 						<div class="form-group">
 							<div class="col-md-offset-2 col-md-8">
-								<label>Parecer artístico*</label>
+								<label>Parecer artístico****</label>
 								<textarea name="parecerArtistico" class="form-control" rows="10" placeholder="Texto usado fins jurídicos e confecção de contratos."><?php echo $pedido["parecerArtistico"] ?></textarea>
 							</div> 
-						</div>		
+						</div>	
 		<?php
 			}
 			else
@@ -1489,9 +1489,14 @@
 									<textarea name='parecerArtistico' class='form-control' rows='10' placeholder='Texto usado fins jurídicos e confecção de contratos.'></textarea>
 								</div> 
 							</div>
+							<div class='form-group'>
+							<div class='col-md-offset-2 col-md-8'>
+								<a href='?perfil=contratados&p=edicaoParecer' class='btn btn-theme btn-block'>Editar parecer</a>
+							</div>
+						</div>
 						"; 
 				}
-			}
+			}	
 		?>							
 						<div class="form-group">
 							<div class="col-md-offset-2 col-md-8"><strong>Observação:</strong><br/>
@@ -3465,6 +3470,51 @@
 		</div>
 	</div>
 </section>
+	<?php 
+		break;
+		case "edicaoParecer":
+	?>	
+<section id="contact" class="home-section bg-white">
+	<div class="container">
+		<div class="form-group">
+			<h2>PARECER PJ artista Local</h2>
+            <p><?php if(isset($mensagem)){echo $mensagem;} ?></p>			
+		</div>
+	  	<div class="row">
+	  		<div class="col-md-offset-1 col-md-10">    
+                <div class="form-group">					
+					<hr/>
+					<h6>1º Tópico</h6>
+					<label>Neste tópico deve conter o posicionamento da comissão e as informações gerais do evento (nome do artista, evento, datas, valor, tempo, etc).</label>
+					<p align="justify"><font color="gray"><strong><i>Texto de exemplo:</strong><br/>Esta comissão ratifica o pedido de contratação de Nome do artista ou grupo(nome artístico) por intermédio da Nome da empresa representante, para apresentação artística no evento “Nome do evento ou atividade especial”, que ocorrerá no dia datas ou período quando for temporada no valor de R$ XXX (valor por extenso).</font></i></p>
+					<textarea name="topico1" class="form-control" rows="6"><?php //echo $pedido["justificativa"] ?></textarea>
+					
+					<hr/>
+					
+					<h6>2º Tópico (mínimo de 500 caracteres)</h6> 
+					<label>Neste tópico deve-se falar sobre o evento ou atividade especial da qual o artista/grupo irá participar. Se for programação geral do equipamento, sem estar vinculada a nenhum evento ou projeto específico, falar sobre o equipamento, histórico, tipo de atividades desenvolvidas, etc, demonstrando a importância desse tipo de programação dentro do equipamento.</label>
+					<p align="justify"><font color="gray"><strong><i>Texto de exemplo:</strong><br/>Em sua nona edição, o projeto Virada Cultural, da Secretaria Municipal de Cultura, consolida a Cidade de São Paulo como o principal pólo gerador de arte e cultura do País proporcionando, não só aos munícipes como também aos visitantes de outros Estados e de outras nacionalidades, o acesso gratuito ao que há de melhor na produção cultural atual existente no Brasil e no exterior. A Virada Cultural da Cidade de São Paulo, através de apresentações artísticas em logradouros públicos e equipamentos oficiais dentre outros espaços culturais conquistou, nesses nove anos de existência, o reconhecimento da mídia e do público, solidificando-se como um dos eventos nacionais mais conhecidos e divulgados do Brasil, assim como no exterior.</font></i></p>
+					<textarea name="topico2" class="form-control" rows="10"><?php //echo $pedido["justificativa"] ?></textarea>
+					
+					<hr/>
+					
+					<h6>3º Tópico (mínimo de 700 caracteres)</h6> 
+					<label>Neste tópico deve-se falar sobre o currículo/biografia do artista ou grupo (na 3ª pessoa), escrever um breve release. Deve ficar claro que o artista contribuirá positivamente para a programação e porque essa é a melhor escolha de artista para o evento.</label>
+					<textarea name="topico3" class="form-control" rows="10"><?php //echo $pedido["justificativa"] ?></textarea>
+					
+					<hr/>
+					
+					<h6>4º Tópico</h6> 
+					<label>Neste tópico deve-se falar que o contratado tem o necessário para a contratação e que as exigências legais foram observadas, apresentando a comprovação documental (mínimo três comprovações diferentes) do valor proposto para o cachê. Encerrar com a manifestação favorável da comissão quanto à contratação.</label>
+					<p align="justify"><font color="gray"><strong><i>Texto de exemplo:</strong><br/>O contratado, pelo exposto, reúne as condições necessárias para desenvolver este trabalho de natureza artística, tendo em vista que uma das metas da Secretaria Municipal de Cultura é proporcionar uma oportunidade para artistas que ainda não se projetaram na carreira e tenham visibilidade local. Ainda, avaliamos que o cachê proposto encontra-se compatível com os valores praticados no mercado e pagos por esta Secretaria, conforme pode ser comprovado pelos documentos x, y e z, em cumprimento ao Acórdão TCM 2.393/15-37.
+					<br/>
+					Sendo os serviços indubitavelmente de natureza artística, manifestamo-nos favoravelmente à contratação, endossando a proposta inicial.</font></i></p>
+					<textarea name="topico2" class="form-control" rows="10"><?php //echo $pedido["justificativa"] ?></textarea>											
+				</div>
+			</div>
+		</div>
+	</div>
+</section>	
 	<?php 
 		break;
 	} //fim da switch
