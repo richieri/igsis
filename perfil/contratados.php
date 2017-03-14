4c31110a018012f1ba7e4202449540b77ba8c4e8
LoreleiGab <loreleigab@hotmail.com>
2017-03-14 14:57:30 -0300

    Correção nadata das parcelas

--- perfil/contratados.php
+++ perfil/contratados.php
@@ -2427,7 +2427,7 @@
 							<input type='text'  name="valor<?php echo $i; ?>" id='valor' class='form-control valor' value="<?php echo dinheiroParaBr($parcela['valor']); ?>">
 						</div>
 						<div class="col-xs-6 col-sm-3"><strong>Data do Kit de Pagamento:</strong><br/>
-							<input type='text' name="data<?php echo $i; ?>" id='' class='form-control datepicker' value="
+							<input type='text' name="data<?php echo $i; ?>" id='datepicker1<?php echo $i; ?>' class='form-control datepicker' value="
 			<?php 
 				if($parcela['vencimento'] == '0000-00-00 00:00:00' OR $parcela['vencimento'] == NULL)
 				{ 
