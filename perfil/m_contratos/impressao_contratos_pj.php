5fda35377358a1104374d4e5c8b08d944d80ccd8
LoreleiGab <loreleigab@hotmail.com>
2017-04-26 13:25:58 -0300

    Proposta Virada 2017

--- perfil/m_contratos/impressao_contratos_pj.php
+++ perfil/m_contratos/impressao_contratos_pj.php
@@ -56,6 +56,7 @@ $link46 = $http."rlt_pedido_reserva_dph_pj.php";
 $link47 = $http."rlt_pedido_reserva_csmb_pj.php";
 $link48 = $http."rlt_pedido_reserva_casasdecultura_pj.php";
 $link49 = $http."rlt_pedido_reserva_ccsp_pj.php";
+$link50 = $http."rlt_proposta_virada.php"."?id=".$id_ped."&penal=30";
 
 
 
@@ -104,6 +105,9 @@ echo "
 						<tr>
 							<td><a href='$link36?id=$id_ped' target='_blank'><strong>Edital - Gala de Balé</strong></a></td>
 							<td><a href='$link32' target='_blank'><strong>NORMAS INTERNAS - Teatros Municipais</strong></a></td>
+						</tr>
+						<tr>
+							<td><a href='$link50' target='_blank'><strong>Virada 2017</strong></a></td>
 						</tr>
 						
 						<tr><td class='list_description'><br/></td></tr>
