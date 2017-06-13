d1fe0060a3433e3c9edd10c71e121777d3456a7e
LoreleiGab <loreleigab@hotmail.com>
2017-06-13 12:05:21 -0300

    Ajustes proposta reversão curta PJ

--- pdf/rlt_proposta_reversaocurta_pj.php
+++ pdf/rlt_proposta_reversaocurta_pj.php
@@ -310,29 +310,36 @@ $l=7; //DEFINE A ALTURA DA LINHA
    
    $pdf->SetX($x);
    $pdf->SetFont('Arial','B', 10);
-   $pdf->Cell(40,$l,utf8_decode('Valor da Prestação do Serviço:'),0,1,'L');
+   $pdf->Cell(40,5,utf8_decode('Valor da Prestação do Serviço:'),0,1,'L');
    
    $pdf->SetX($x);
    $pdf->SetFont('Arial','', 10);
-   $pdf->MultiCell(140,$l,utf8_decode($FormaPagamento));
+   $pdf->MultiCell(180,5,utf8_decode($FormaPagamento));
    
    $pdf->SetX($x);
    $pdf->SetFont('Arial','B', 10);
-   $pdf->Cell(25,$l,'Justificativa:',0,0,'L');
+   $pdf->Cell(25,5,'Justificativa:',0,0,'L');
    $pdf->SetFont('Arial','', 10);
-   $pdf->MultiCell(155,$l,utf8_decode($Justificativa));
+   $pdf->MultiCell(155,5,utf8_decode($Justificativa));
 
 
 //RODAPÉ PERSONALIZADO
    $pdf->SetXY($x,262);
    $pdf->SetFont('Arial','', 10);
-   $pdf->Cell(85,$l,utf8_decode($rep01Nome),'T',0,'L');
-   $pdf->Cell(85,$l,utf8_decode($rep02Nome),'T',1,'L');
+   $pdf->Cell(85,5,utf8_decode($rep01Nome),'T',0,'L');
+   if ($rep02Nome != '')
+   {
+	   $pdf->Cell(85,5,utf8_decode($rep02Nome),'T',0,'L');
+   }  
+   $pdf->Ln();
    
    $pdf->SetX($x);
    $pdf->SetFont('Arial','', 10);
-   $pdf->Cell(85,$l,"RG: ".$rep01RG,0,0,'L');
-   $pdf->Cell(85,$l,"RG: ".$rep02RG,0,0,'L');
+   $pdf->Cell(85,5,"RG: ".$rep01RG,0,0,'L');
+   if ($rep02Nome != '')
+   {
+	   $pdf->Cell(85,5,"RG: ".$rep02RG,0,0,'L');
+   } 
    
 
 //	QUEBRA DE PÁGINA
@@ -344,7 +351,7 @@ $l=4; //DEFINE A ALTURA DA LINHA
 
 	//Pessoa Jurídica
 
-$pdf->SetX($x);
+   $pdf->SetX($x);
    $pdf->SetFont('Arial','', 10);
    $pdf->Cell(10,5,'(C)',0,0,'L');
    $pdf->SetFont('Arial','B', 10);
@@ -453,24 +460,23 @@ $pdf->SetX($x);
 
 $l=3.5; //DEFINE A ALTURA DA LINHA
 
-$pdf->SetX($x);
+   $pdf->SetX($x);
    $pdf->SetFont('Arial','', 8);
    $pdf->Cell(10,$l,'(D)',0,0,'L');
    $pdf->SetFont('Arial','B', 8);
    $pdf->Cell(170,5,utf8_decode('OBSERVAÇÃO'),0,1,'C');
    
-$pdf->SetX($x);
-$pdf->PrintChapter('txt/proposta_reversaocurta_observacao.txt');
+   $pdf->SetX($x);
+   $pdf->PrintChapter('txt/proposta_reversaocurta_observacao.txt');
     
-
-$pdf->SetX($x);
+   $pdf->SetX($x);
    $pdf->SetFont('Arial','', 8);
    $pdf->Cell(10,$l,'',0,0,'L');
    $pdf->SetFont('Arial','B', 8);
    $pdf->Cell(170,5,utf8_decode('DECLARAÇÕES'),0,1,'C');
    
    $pdf->SetX($x);
-   $pdf->SetFont('Arial','', 8);
+   $pdf->SetFont('Arial','', 7);
    $pdf->MultiCell(0,$l,utf8_decode($txtPenalidade),0,'J');
     
    $pdf->Ln();
@@ -489,13 +495,20 @@ $pdf->SetX($x);
 //RODAPÉ PERSONALIZADO
    $pdf->SetXY($x,262);
    $pdf->SetFont('Arial','', 10);
-   $pdf->Cell(85,$l,utf8_decode($rep01Nome),'T',0,'L');
-   $pdf->Cell(85,$l,utf8_decode($rep02Nome),'T',1,'L');
+   $pdf->Cell(85,5,utf8_decode($rep01Nome),'T',0,'L');
+   if ($rep02Nome != '')
+   {
+	   $pdf->Cell(85,5,utf8_decode($rep02Nome),'T',0,'L');
+   }  
+   $pdf->Ln();
    
    $pdf->SetX($x);
    $pdf->SetFont('Arial','', 10);
-   $pdf->Cell(85,$l,"RG: ".$rep01RG,0,0,'L');
-   $pdf->Cell(85,$l,"RG: ".$rep02RG,0,0,'L');
+   $pdf->Cell(85,5,"RG: ".$rep01RG,0,0,'L');
+   if ($rep02Nome != '')
+   {
+	   $pdf->Cell(85,5,"RG: ".$rep02RG,0,0,'L');
+   } 
    
    
    
@@ -556,13 +569,20 @@ $l=5; //DEFINE A ALTURA DA LINHA
 //RODAPÉ PERSONALIZADO
    $pdf->SetXY($x,262);
    $pdf->SetFont('Arial','', 10);
-   $pdf->Cell(85,$l,utf8_decode($rep01Nome),'T',0,'L');
-   $pdf->Cell(85,$l,utf8_decode($rep02Nome),'T',1,'L');
+   $pdf->Cell(85,5,utf8_decode($rep01Nome),'T',0,'L');
+   if ($rep02Nome != '')
+   {
+	   $pdf->Cell(85,5,utf8_decode($rep02Nome),'T',0,'L');
+   }  
+   $pdf->Ln();
    
    $pdf->SetX($x);
    $pdf->SetFont('Arial','', 10);
-   $pdf->Cell(85,$l,"RG: ".$rep01RG,0,0,'L');
-   $pdf->Cell(85,$l,"RG: ".$rep02RG,0,0,'L');
+   $pdf->Cell(85,5,"RG: ".$rep01RG,0,0,'L');
+   if ($rep02Nome != '')
+   {
+	   $pdf->Cell(85,5,"RG: ".$rep02RG,0,0,'L');
+   } 
    
 
 //for($i=1;$i<=20;$i++)
