46ad6557684402966d849ff3a98957ae62145d29
LoreleiGab <loreleigab@hotmail.com>
2017-06-19 15:23:42 -0300

    Ajustes no menu de comunicação beta para emissão de relatório

--- perfil/m_comunicacao_beta/include/menu.php
+++ perfil/m_comunicacao_beta/include/menu.php
@@ -2,6 +2,10 @@
 //geram o insert pro framework da igsis
 $pasta = "?perfil=comunicacao_beta&p=";
 $perfil = $_SESSION['perfil'];
+
+$server = "http://".$_SERVER['SERVER_NAME']."/igsis/";
+$http = $server."/pdf/";
+$link = $http."rlt_comunicacao_fotos.php";
 ?>
 
 <div class="menu-area">
@@ -10,8 +14,8 @@ $perfil = $_SESSION['perfil'];
 			<ul class="dl-menu">				
 				<li><a href="<?php echo $pasta ?>filtro">Filtrar</a></li>
 				<li><a href="<?php echo $pasta ?>programacao_local">Programação Local</a></li>
-				<li><a href="<?php echo $pasta ?>foto">Foto/Imagem de Divulgação</a></li>
-				<li><a href="<?php echo $pasta ?>docs">Gerar Docs</a></li>   
+				<li><a href='<?php echo $link ?>' target='_blank'>Relatório de Foto do Mês</a></li>
+				<li><a href="<?php echo $pasta ?>docs">Em Cartaz</a></li>   
 				<li><a href="<?php echo $pasta ?>programacao_local">Agenda</a></li>				
 				<li><a href="<?php echo $pasta ?>chamados">Lista de chamados</a></li> 			
 				<li style="color:white;">-------------------------</li>
