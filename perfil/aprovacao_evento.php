7a46a33cc7e436c8a5473ac638ae20a3e30128e1
Lorelei Gabriele <loreleigab@hotmail.com>
2017-09-18 22:01:04 -0300

    Começo da listagem do texto para envio do email

--- perfil/aprovacao_evento.php
+++ perfil/aprovacao_evento.php
@@ -19,8 +19,23 @@
 		if($query_atualiza_evento)
 		{
 			gravarLog($sql_atualiza_pedido);
+			$i = 0;
+			$sql = "SELECT * FROM igsis_pedido_contratacao WHERE `idEvento` = '$idEvento'";
+			$query = mysqli_query($con,$sql);
+			while($ped = mysqli_fetch_array($query))
+			{
+				$pedido = recuperaDados("igsis_pedido_contratacao",$ped['idPedidoContratacao'],"idPedidoContratacao");
+				$evento = recuperaDados("ig_evento",$pedido['idEvento'],"idEvento"); //$tabela,$idEvento,$campo
+				$x[$i]['idEvento'] = $evento['idEvento'];
+				$x[$i]['idPedido']= $pedido['idPedidoContratacao'];
+				$x[$i]['nomeEvento'] = $evento['nomeEvento'];
+				$i++;				
+			}
 			$mensagem = "<h3>Solicitação enviada!</h3>
-				<p>Entre em contato através do email <strong>pedidosdecontratacao@gmail.com</strong> informando o <strong>Nº da IG, Nome do Evento </strong> e <strong>Número(s) do(s) Pedido(s) de Contratação.</strong></p>";
+				<p>Entre em contato através do email <strong>pedidosdecontratacao@gmail.com</strong> informando o <strong>Nº da IG, Nome do Evento </strong> e <strong>Número(s) do(s) Pedido(s) de Contratação.</strong></p>
+				<p></p>
+				";
+			
 		}
 		else
 		{
@@ -38,6 +53,16 @@
 					<p><?php //var_dump($_SESSION); ?></p>
 					<?php if(isset($mensagem)){echo $mensagem;} ?>
 					<p>&nbsp;</p>
+					<?php
+					for($h = 0; $h < $i; $h++)
+					{
+						echo '<p>';
+						echo '<strong>Número do Evento:</strong> '.$x[$h]['idEvento'].'<br/>';
+						echo '<strong>Nome do Evento:</strong> '.$x[$h]['nomeEvento'].'<br/>';
+						echo '<strong>Número do Pedido:</strong> '.$x[$h]['idPedido'];
+						echo'</p>';
+					}
+					?>
 					<p><a href="?perfil=evento">Voltar ao módulo eventos?</a></p>
 				</div>
 			</div>
