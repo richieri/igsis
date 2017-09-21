98f2fb0e62adfd708b060b7cb814d8128623a552
LoreleiGab <loreleigab@hotmail.com>
2017-09-21 13:41:50 -0300

    mudanças

--- perfil/contratados_migracao.php
+++ perfil/contratados_migracao.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 	/*
 	Para fazer
 	+ funcao que retornam os locais
@@ -164,12 +164,12 @@
 						}
 						else
 						{
-							echo "<h1>Erro ao inserir!</h1>";
+							echo "<h1>Erro ao inserir![1]</h1>";
 						}
 					}
 					else
 					{
-						echo "<h1>Erro ao inserir!</h1>";
+						echo "<h1>Erro ao inserir![2]</h1>";
 					}
 				}
 			}
@@ -729,240 +729,40 @@
 			}
 		break;
 		case 'fisica':
-			if(isset($_POST['pesquisar']))
-			{
-				// inicia a busca por Razao Social ou CNPJ
-				$busca = $_POST['busca'];
-				$sql_busca = "SELECT * FROM sis_pessoa_fisica WHERE CPF = '$busca' ORDER BY Nome";
-				$query_busca = mysqli_query($con,$sql_busca); 
-				$num_busca = mysqli_num_rows($query_busca);
-				if($num_busca > 0)
-				{
-					// Se exisitr, lista a resposta.
-		?>
-<section id="services" class="home-section bg-white">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-offset-2 col-md-8">
-				<div class="section-heading">
-					<h2>Contratados - Pessoa Física</h2>                      
-					<p></p>
+
+	// Se não existe pedido de busca, exibe campo de pesquisa.
+?>    
+	<section id="services" class="home-section bg-white">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-offset-2 col-md-8">
+					<div class="section-heading">
+						<h2>Contratados - Pessoa Física</h2>
+						<p>Você está inserindo pessoas físicas para serem contratadas para o evento <strong><?php  echo $nomeEvento['nomeEvento']; ?></strong></p>
+						<p></p>
+					</div>
+				</div>
+			</div>	  
+			<div class="row">
+				<div class="form-group">
+					<div class="col-md-offset-2 col-md-8">
+						<form method="POST" action="?perfil=compara_pf" class="form-horizontal" role="form">
+							<label>Insira o CPF</label>
+							<input type="text" name="busca" class="form-control" id="cpf" >
+							<br />             
+							<div class="form-group">
+								<div class="col-md-offset-2 col-md-8">
+									<input type="hidden" name="pesquisar" value="1" />
+									<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
+								</div>
+							</div>	
+						</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</section>
-<section id="list_items" class="home-section bg-white">
-	<div class="container">
-		<div class="table-responsive list_info">
-			<table class="table table-condensed">
-				<thead>
-					<tr class="list_menu">
-						<td>Nome</td>
-						<td>CPF</td>
-						<td width="25%"></td>
-						<td width="5%"></td>
-					</tr>
-				</thead>
-				<tbody>
-				<?php
-					while($descricao = mysqli_fetch_array($query_busca))
-					{			
-						echo "<tr>";
-						echo "<td class='list_description'><b>".$descricao['Nome']."</b></td>";
-						echo "<td class='list_description'>".$descricao['CPF']."</td>";
-						echo "
-							<td class='list_description'>
-							<form method='POST' action='?perfil=contratados&p=lista'>
-							<input type='hidden' name='insereFisica' value='1'>
-							<input type='hidden' name='Id_PessoaFisica' value='".$descricao['Id_PessoaFisica']."'>
-							<input type ='submit' class='btn btn-theme btn-md btn-block' value='inserir'></td></form>"	;
-						echo "</tr>";
-					}
-				?>		
-				</tbody>
-			</table>
-		</div>
-	</div>
-</section>
-			<?php
-				}
-				else
-				{
-					// Se não existe, exibe um formulario para insercao.
-					$ultimo = cadastroPessoa($_SESSION['idEvento'],$CPF,'1'); 
-					$campo = recuperaDados("sis_pessoa_fisica",$ultimo,"Id_PessoaFisica");
-			?>
-<section id="contact" class="home-section bg-white">
-	<div class="container">
-		<div class="form-group">
-			<h3>CADASTRO DE PESSOA FÍSICA</h3>
-			<p> O CPF <?php echo $busca; ?> não está cadastrado no nosso sistema. <br />Por favor, insira as informações da Pessoa Física a ser contratada. </p>
-			<p><a href="?perfil=contratados&p=fisica"> Pesquisar outro CPF</a> </p>
-		</div>
-	  	<div class="row">
-	  		<div class="col-md-offset-1 col-md-10">
-				<form class="form-horizontal" role="form" action="?perfil=contratados&p=lista" method="post">
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><strong>Nome *:</strong><br/>
-							<input type="text" class="form-control" id="Nome" name="Nome" placeholder="Nome" >
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><strong>Nome Artístico:</strong><br/>
-							<input type="text" class="form-control" id="NomeArtistico" name="NomeArtistico" placeholder="Nome Artístico" >
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>Tipo de documento *:</strong><br/>
-							<select class="form-control" id="tipoDocumento" name="tipoDocumento" >
-								<?php geraOpcao("igsis_tipo_documento","",""); ?>  
-							</select>
-						</div>				  
-						<div class=" col-md-6"><strong>Documento *:</strong><br/>
-							<input type="text" class="form-control" id="RG" name="RG" placeholder="Documento" >
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>CPF *:</strong><br/>
-							<input type="text" class="form-control" id="cpf" name="CPF" placeholder="CPF" value="<?php echo $busca; ?> ">
-						</div>				  
-						<div class=" col-md-6"><strong>CCM *:</strong><br/>
-							<input type="text" class="form-control" id="CCM" name="CCM" placeholder="CCM" >
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>Estado civil:</strong><br/>
-							<select class="form-control" id="IdEstadoCivil" name="IdEstadoCivil" >
-								<?php geraOpcao("sis_estado_civil","",""); ?>  
-							</select>
-						</div>				  
-						<div class=" col-md-6"><strong>Data de nascimento:</strong><br/>
-							<input type="text" class="form-control" id="datepicker01" name="DataNascimento" placeholder="Data de Nascimento" >
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>Nacionalidade:</strong><br/>
-							<input type="text" class="form-control" id="Nacionalidade" name="Nacionalidade" placeholder="Nacionalidade">
-						</div>				  
-						<div class=" col-md-6"><strong>CEP:</strong><br/>
-							<input type="text" class="form-control" id="CEP" name="CEP" placeholder="CEP">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><strong>Endereço *:</strong><br/>
-							<input type="text" class="form-control" id="Endereco" name="Endereco" placeholder="Endereço">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>Número *:</strong><br/>
-							<input type="text" class="form-control" id="Numero" name="Numero" placeholder="Numero">
-						</div>				  
-						<div class=" col-md-6"><strong>Bairro:</strong><br/>
-							<input type="text" class="form-control" id="Bairro" name="Bairro" placeholder="Bairro">
-						</div>
-					</div>
-					<div class="form-group">     
-						<div class="col-md-offset-2 col-md-8"><strong>Complemento *:</strong><br/>
-							<input type="text" class="form-control" id="Complemento" name="Complemento" placeholder="Complemento">
-						</div>
-					</div>		
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>Cidade *:</strong><br/>
-							<input type="text" class="form-control" id="Cidade" name="Cidade" placeholder="Cidade">
-						</div>
-						<div class=" col-md-6"><strong>Estado *:</strong><br/>
-							<input type="text" class="form-control" id="Estado" name="Estado" placeholder="Estado">
-						</div>
-					</div>		  
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>E-mail *:</strong><br/>
-							<input type="text" class="form-control" id="Email" name="Email" placeholder="E-mail" >
-						</div>				  
-						<div class=" col-md-6"><strong>Telefone #1 *:</strong><br/>
-							<input type="text" class="form-control" id="telefone" onkeyup="mascara( this, mtel );" maxlength="15" name="Telefone1" placeholder="Exemplo: (11) 98765-4321" >
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>Telefone #2:</strong><br/>
-							<input type="text" class="form-control" id="telefone" onkeyup="mascara( this, mtel );" maxlength="15" name="Telefone2" placeholder="Exemplo: (11) 98765-4321" >
-						</div>				  
-						<div class="col-md-6"><strong>Telefone #3:</strong><br/>
-							<input type="text" class="form-control" id="telefone" onkeyup="mascara( this, mtel );" maxlength="15" name="Telefone3" placeholder="Exemplo: (11) 98765-4321" >
-						</div>
-					</div>	  
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>DRT:</strong><br/>
-							<input type="text" class="form-control" id="DRT" name="DRT" placeholder="DRT" >
-						</div>				  
-						<div class=" col-md-6"><strong>Função:</strong><br/>
-							<input type="text" class="form-control" id="Funcao" name="Funcao" placeholder="Função">
-						</div>
-					</div>  
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-6"><strong>Inscrição do INSS ou PIS/PASEP:</strong><br/>
-							<input type="text" class="form-control" id="InscricaoINSS" name="InscricaoINSS" placeholder="Inscrição no INSS ou PIS/PASEP" >
-						</div>				  
-						<div class=" col-md-6"><strong>OMB:</strong><br/>
-							<input type="text" class="form-control" id="OMB" name="OMB" placeholder="OMB" >
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8"><strong>Observação:</strong><br/>
-							<textarea name="Observacao" class="form-control" rows="10" placeholder=""></textarea>
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-md-offset-2 col-md-8">
-							<input type="hidden" name="cadastrarFisica" value="1" />
-							<input type="hidden" name="Sucesso" id="Sucesso" />
-							<input type="submit" value="GRAVAR" class="btn btn-theme btn-lg btn-block">
-						</div>
-					</div>
-				</form>
-	  		</div>	
-	  	</div>
-	</div>
-</section>
-			<?php
-				} 
-			}
-			else
-			{
-				// Se não existe pedido de busca, exibe campo de pesquisa.
-			?>    
-<section id="services" class="home-section bg-white">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-offset-2 col-md-8">
-				<div class="section-heading">
-					<h2>Contratados - Pessoa Física</h2>
-                    <p>Você está inserindo pessoas físicas para serem contratadas para o evento <strong><?php  echo $nomeEvento['nomeEvento']; ?></strong></p>
-					<p></p>
-				</div>
-			</div>
-		</div>	  
-	    <div class="row">
-            <div class="form-group">
-            	<div class="col-md-offset-2 col-md-8">
-					<form method="POST" action="?perfil=contratados&p=fisica" class="form-horizontal" role="form">
-						<label>Insira o CPF</label>
-						<input type="text" name="busca" class="form-control" id="cpf" >
-						<br />             
-						<div class="form-group">
-							<div class="col-md-offset-2 col-md-8">
-								<input type="hidden" name="pesquisar" value="1" />
-								<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
-							</div>
-						</div>	
-					</form>
-        	    </div>
-        	</div>
-        </div>
-	</div>
-</section>
-<?php
-			}
+	</section>
+<?php			
 		break;
 		case 'representante':
 			if(isset($_POST['numero']))
@@ -3941,7 +3741,6 @@ function mostrarResultado(box,num_max,campospan){
 		document.getElementById(campospan).innerHTML = "Ainda não temos nada digitado...";
 	}
 }
-
 function contarCaracteres(box,valor,campospan){
 	var conta = valor - box.length;
 	document.getElementById(campospan).innerHTML = "Faltam " + conta + " caracteres";
@@ -3949,7 +3748,6 @@ function contarCaracteres(box,valor,campospan){
 		document.getElementById(campospan).innerHTML = "Quantidade mínima de caracteres atingida!";
 	}	
 }
-
 function mostrarResultado3(box,num_max,campospan){
 	var contagem_carac = box.length;
 	if (contagem_carac != 0){
