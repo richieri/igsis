af6ac7e368c225745ec0d9ba90c4f24c0f4dc19f
LoreleiGab <loreleigab@hotmail.com>
2017-10-11 17:01:06 -0300

    Customização do rodapé

--- visual/rodape.php
+++ visual/rodape.php
@@ -1,32 +1,35 @@
 
 
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-                	<p><?php geraFrase(); ?></p>
-					<p><img src="visual/images/logo_cultura.png" />
-						<img src="images/logo_cultura.png" /><br/>
-						2015 @ Informações Gerais CCSP + Siscontrat DEC / Secretaria Municipal de Cultura / Prefeitura de São Paulo
-					</p>
-				</div>
-				<div class="col-md-12">
-					<?php
-if($_SESSION['perfil'] == 1){
-echo "<strong>SESSION</strong><pre>", var_dump($_SESSION), "</pre>";
-echo "<strong>POST</strong><pre>", var_dump($_POST), "</pre>";
-echo "<strong>GET</strong><pre>", var_dump($_GET), "</pre>";
-echo "<strong>SERVER</strong><pre>", var_dump($_SERVER), "</pre>";
-echo "<strong>FILES</strong><pre>", var_dump($_FILES), "</pre>";
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<p><?php geraFrase(); ?></p>
+				<table width="100%">
+					<tr>
+						<td width="20%"><img src="images/logo_cultura_q.png" align="left"/></td>
+						<td align="center"><font color="#ccc">2017 @ IGSIS - Cadastro de Artistas e Profissionais de Arte e Cultura<br/>Secretaria Municipal de Cultura<br/>Prefeitura de São Paulo</font></td>
+						<td width="20%"><img src="images/logo_igsis_azul.png" align="right"/></td>
+					</tr>
+				</table>
+			</div>
+			<div class="col-md-12">
+				<?php
+				if($_SESSION['perfil'] == 1)
+				{
+					echo "<strong>SESSION</strong><pre>", var_dump($_SESSION), "</pre>";
+					echo "<strong>POST</strong><pre>", var_dump($_POST), "</pre>";
+					echo "<strong>GET</strong><pre>", var_dump($_GET), "</pre>";
+					echo "<strong>SERVER</strong><pre>", var_dump($_SERVER), "</pre>";
+					echo "<strong>FILES</strong><pre>", var_dump($_FILES), "</pre>";
 
-echo ini_get('session.gc_maxlifetime')/60; // em minutos
-}
-
-?>
-				</div>
-			</div>		
-		</div>	
-	</footer>
+					echo ini_get('session.gc_maxlifetime')/60; // em minutos
+				}
+				?>
+			</div>
+		</div>		
+	</div>	
+</footer>
 	 
 	 <!-- js -->
     <!--<script src="js/jquery.js"></script>-->
