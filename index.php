b292cbeacc7626aa3741106659b3f230dd84b0f9
LoreleiGab <loreleigab@hotmail.com>
2017-10-16 10:18:59 -0200

    Mudanças na página inicial

--- index.php
+++ index.php
@@ -62,18 +62,17 @@ if(isset($_POST['usuario']))
 		<script src="visual/js/modernizr.custom.js"></script>
 	</head>
 	<body>
-		<section id="contact" class="home-section bg-white">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-offset-2 col-md-8">
-						<div class="text-hide">
-							<h2>IGSIS - SMC</h2>
-							<h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
-						</div>
-					</div>
+		<section id="spacer1" class="home-section spacer">	
+           <div class="container">
+				<div class="color-light">
+					<h1 class="wow bounceInDown" data-wow-delay="1s">IGSIS - SMC</h1>							
 				</div>
-				<div class="row">
-					<div class="col-md-offset-1 col-md-10">
+            </div>
+		</section>
+		<section id="contact" class="home-section bg-white">
+			<div class="container">			
+				<div class="col-md-offset-1 col-md-10">	
+					<h5><?php if(isset($mensagem)){ echo $mensagem; } ?></h5>
 					<form method="POST" action="index.php"class="form-horizontal" role="form">
 						<div class="form-group">
 							<div class="col-md-offset-2 col-md-6">
@@ -92,52 +91,20 @@ if(isset($_POST['usuario']))
 					</form>
 					
 					<br /><br />
-					
-						<div class="form-group">
-							<div class="col-md-offset-2 col-md-8">
-								<h4><p>Não possui cadastro?<strong><a href="https://goo.gl/lHLdnI" target="_blank"> CLIQUE AQUI!</strong></a></p></h4>
-								<p>&nbsp;</p>
-								<p>Dúvidas? Envie e-mail para: <strong>sistema.igsis@gmail.com</strong></p>
-								<br />
-							</div>
+				
+					<div class="form-group">
+						<div class="col-md-offset-2 col-md-8">
+							<h4><p>Não possui cadastro?<strong><a href="https://goo.gl/lHLdnI" target="_blank"> CLIQUE AQUI!</strong></a></p></h4>
+							<p>&nbsp;</p>
+							<p>Dúvidas? Envie e-mail para: <strong>sistema.igsis@gmail.com</strong></p>
 						</div>
-						
-						<!--
-						<div class="form-group">
-							<div class="col-md-offset-2 col-md-8">
-								<h5>Últimos eventos inseridos</h5>
-								<div class="left">
-									<ul>
-									<?php
-										$con = bancoMysqli();
-										$sql_ultimo = "SELECT * FROM ig_evento WHERE dataEnvio IS NOT NULL ORDER BY dataEnvio DESC LIMIT 0,20";
-										$query_ultimo = mysqli_query($con,$sql_ultimo);
-										while($evento = mysqli_fetch_array($query_ultimo))
-										{
-											$usuario = recuperaUsuario($evento['idUsuario']);
-											$instituicao = recuperaDados("ig_instituicao",$usuario['idInstituicao'],"idInstituicao");	
-										?>
-											<li><p><strong><?php echo $evento['nomeEvento'] ?> </strong>(<?php echo retornaTipo($evento['ig_tipo_evento_idTipoEvento']) ?>) </p>
-											<p><i><?php echo $evento['autor'] ?></i> - enviado por: <?php echo $usuario['nomeCompleto'] ?> (<?php echo $instituicao['instituicao'] ?>) em: <?php echo exibirDataBr($evento['dataEnvio']) ?></p>
-											<p><?php echo resumoOcorrencias($evento['idEvento']); ?></p>
-											<br />				
-											</li>
-									<?php 
-										} 
-									?>
-									</ul>                   
-								</div>
-							</div>
-						</div>
-						-->
 					</div>
-				</div>
+				</div>				
 			</div>
 		</section>   
 		<footer>
 			<div class="container">
-				<div class="row">					
-					<p><?php geraFrase(); ?></p>
+				<div class="row">
 					<table width="100%">
 						<tr>
 							<td width="20%"><img src="visual/images/logo_cultura_q.png" align="left"/></td>
