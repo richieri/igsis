ae24e5a8d3b51ab93798b46aae470605b12a0da6
LoreleiGab <loreleigab@hotmail.com>
2017-10-17 17:04:59 -0200

    Filtro por periodo e operador no módulo contratos

--- perfil/m_contratos/includes/menu.php
+++ perfil/m_contratos/includes/menu.php
@@ -22,6 +22,7 @@ $pasta = "?perfil=contratos&p=";
 			</li>
 			<li><a href="<?php echo $pasta ?>frm_evento_sem_reenvio">Eventos Sem Reenvios</a></li>
 			<li><a href="<?php echo $pasta ?>frm_busca_periodo">Filtro por Período</a></li>
+			<li><a href="<?php echo $pasta ?>frm_busca_periodo_operador">Filtro por Período/Operador</a></li>
 			<li><a href="<?php echo $pasta ?>frm_busca_dataenvio">Filtro por Data de Envio</a></li>
 			<li><a href="#">Especiais</a>
 				<ul class="dl-submenu">
