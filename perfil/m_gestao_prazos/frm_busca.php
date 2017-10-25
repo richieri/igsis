a462bbfb300184a6eff2f2b5d293693fdfccaad9
LoreleiGab <loreleigab@hotmail.com>
2017-10-25 10:17:25 -0200

    Visualização dos detalhes do pedido na gestão de prazos

--- perfil/m_gestao_prazos/frm_busca.php
+++ perfil/m_gestao_prazos/frm_busca.php
@@ -224,6 +224,7 @@ case 'inicial':
 						<thead>
 							<tr class="list_menu">
 							<td>Id Evento</td>
+							<td>Pedido</td>
 							<td>Objeto</td>
 							<td>Local</td>
 							<td>Periodo</td>
@@ -237,7 +238,8 @@ case 'inicial':
 							
 							for($h = 0; $h < $x['num']; $h++)
 							{		
-								echo "<tr><td class='list_description'> <a href='".$link.$x[$h]['id']."'>".$x[$h]['id']."</a></td>";	
+								echo "<tr><td class='list_description'> <a href='".$link.$x[$h]['id']."'>".$x[$h]['id']."</a></td>";
+								echo "<td class='lista'><a target='_blank'  href='?perfil=contratos&p=frm_edita_propostapj&id_ped=".$x[$h]['id_ped']."'>".$x[$h]['id_ped']."</a></td>";	
 								echo '<td class="list_description">'.$x[$h]['objeto'].				'</td> ';
 								echo '<td class="list_description">'.$x[$h]['local'].				'</td> ';
 								echo '<td class="list_description">'.$x[$h]['periodo'].				'</td> ';
--- perfil/m_gestao_prazos/frm_lista.php
+++ perfil/m_gestao_prazos/frm_lista.php
@@ -31,7 +31,7 @@
 		$pf = recuperaDados("sis_pessoa_fisica",$lista[$i]['IdProponente'],"Id_PessoaFisica");
 		$chamado = recuperaAlteracoesEvento($lista[$i]['idEvento']);	 
 			echo "<tr><td class='lista'> <a href='".$link.$lista[$i]['idEvento']."'>".$lista[$i]['idEvento']."</a></td>";
-			echo "<td class='lista'>" .$lista[$i]['idPedido']."</td>";
+			echo "<td class='lista'><a target='_blank'  href='?perfil=contratos&p=frm_edita_propostapj&id_ped=".$lista[$i]['idPedido']."'>".$lista[$i]['idPedido']."</a></td>";
 			echo '<td class="list_description">'.$pf['Nome'].'</td> ';
 			echo '<td class="list_description">'.$lista[$i]['Objeto'].' [';
 			
