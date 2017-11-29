d3d5caf22d26939f077145c0a3cc301955d2a463
LoreleiGab <loreleigab@hotmail.com>
2017-11-29 17:42:08 -0200

    Página para buscar eventos cadastrados no CAPAC

--- perfil/capac_busca.php
+++ perfil/capac_busca.php
@@ -14,53 +14,24 @@
 				<div class="form-group">
 					<div class="col-md-offset-2 col-md-8">
 						<h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
-						<label>Código do Pedido</label>
-							<input type="text" name="id" class="form-control" id="palavras" placeholder="Insira o Código do Pedido" ><br />
-						<label>ID do Evento</label>
+						<label>Código do cadastro no CAPAC</label>
 							<input type="text" name="idEvento" class="form-control" id="palavras" placeholder="Insira o Código do Evento" ><br />
 						<label>Nome do Evento</label>
 							<input type="text" name="evento" class="form-control" id="palavras" placeholder="Insira o objeto" ><br />
-						<label>Fiscal, suplente ou usuário que cadastrou o evento</label>
-							<select class="form-control" name="fiscal" id="inputSubject" >
-								<option value="0"></option>	
-								<?php echo opcaoUsuario($_SESSION['idInstituicao'],"") ?>
-							</select>
-						<br />
 						<label>Tipo de evento</label>
 							<select class="form-control" name="tipo" id="inputSubject" >
-								<option value="0"></option>		                
-								<?php echo geraOpcao("ig_tipo_evento","","") ?>
-							</select>	
-						<br />
-						<label>Instituição</label>
-							<select class="form-control" name="instituicao" id="inputSubject" >
 								<option value="0"></option>
-								<?php echo geraOpcao("ig_instituicao","","") ?>
-							</select>		
+								<?php echo geraOpcao("ig_tipo_evento","","") ?>
+							</select>
 						<br />
-						<label>Status do pedido</label>
-							<select class="form-control" name="estado" id="inputSubject" >
-								<option value='0'></option>
-								<?php echo geraOpcao("sis_estado","","") ?>
-							</select>		
-						<label>Tipo de Relação Jurídica</label>
-							<select class="form-control" name="juridico" id="inputSubject" >
-								<option value='0'></option>
-								<?php  geraOpcao("ig_modalidade","",""); ?>
-							</select>						
-						<label>Tipo de Projeto</label>
-							<select class="form-control" name="projeto" id="inputSubject" >
-								<option value='0'></option>
-								<?php  geraOpcao("ig_projeto_especial","",""); ?>
-						</select>
 					</div>
 				</div>
-				<br />             
+				<br />
 	            <div class="form-group">
 		            <div class="col-md-offset-2 col-md-8">
 						<input type="hidden" name="pesquisar" value="1" />
-						<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">                    
-        	    	</div>					
+						<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
+        	    	</div>
         	    </div>
 				</form>
 			</div>
