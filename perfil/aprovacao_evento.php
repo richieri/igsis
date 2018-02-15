d924b155f33d5230901fc8975b5a21bd09fe81df
LoreleiGab <loreleigab@hotmail.com>
2018-02-15 16:40:01 -0200

    Remoção do status = 1 antes do envio do evento

--- perfil/aprovacao_evento.php
+++ perfil/aprovacao_evento.php
@@ -12,21 +12,23 @@
 			gravarLog($sql_atualiza_evento);
 			atualizarAgenda($idEvento);
 
-						$titulo = "Reabertura do evento ".$evento['nomeEvento']." pela área de Contratos";
-						$idUsuario = $_SESSION['idUsuario'];
-						$event = $idEvento;
-						$descricao = "Evento fora do prazo!";	
-						$justificativa = "Solicitação de aprovação de evento fora do prazo.";
-						$data = date('Y-m-d H:i:s');
-						
-							$sql_inserir_chamado = "INSERT INTO `igsis_chamado` (`idChamado`, `titulo`, `descricao`, `data`, `idUsuario`, `estado`, `tipo`, `idEvento`, `justificativa`) VALUES (NULL, '$titulo', '$descricao', '$data', '$idUsuario', '1', '13', '$event', '$justificativa')";
-							$query_inserir_chamado = mysqli_query($con,$sql_inserir_chamado);
-										
+			$titulo = "Reabertura do evento ".$evento['nomeEvento']." pela área de Contratos";
+			$idUsuario = $_SESSION['idUsuario'];
+			$event = $idEvento;
+			$descricao = "Evento fora do prazo!";
+			$justificativa = "Solicitação de aprovação de evento fora do prazo.";
+			$data = date('Y-m-d H:i:s');
+
+			$sql_inserir_chamado = "INSERT INTO `igsis_chamado` (`idChamado`, `titulo`, `descricao`, `data`, `idUsuario`, `estado`, `tipo`, `idEvento`, `justificativa`) VALUES (NULL, '$titulo', '$descricao', '$data', '$idUsuario', '1', '13', '$event', '$justificativa')";
+			$query_inserir_chamado = mysqli_query($con,$sql_inserir_chamado);
+
 		}
-		$sql_atualiza_pedido = "UPDATE `igsis`.`igsis_pedido_contratacao` SET 
+		/*
+		$sql_atualiza_pedido = "UPDATE `igsis`.`igsis_pedido_contratacao` SET
 			`estado` = '1'
 			WHERE `igsis_pedido_contratacao`.`idEvento` = '$idEvento' AND `igsis_pedido_contratacao`.`publicado` = '1' ";
 		$query_atualiza_pedido = mysqli_query($con,$sql_atualiza_pedido);
+		*/
 		if($query_atualiza_evento)
 		{
 			gravarLog($sql_atualiza_pedido);
@@ -40,7 +42,7 @@
 				$x[$i]['idEvento'] = $evento['idEvento'];
 				$x[$i]['idPedido']= $pedido['idPedidoContratacao'];
 				$x[$i]['nomeEvento'] = $evento['nomeEvento'];
-				$i++;				
+				$i++;
 			}
 			$mensagem = "<h3>Solicitação enviada!</h3>
 				<p>Entre em contato através do email <strong><a href='mailto:pedidosdecontratacao@gmail.com'>pedidosdecontratacao@gmail.com</a></strong> com as seguintes informações:</p>
@@ -50,7 +52,7 @@
 		}
 		else
 		{
-			$mensagem = "Erro ao enviar formulário! Os seguintes pedidos não foram enviados:";	
+			$mensagem = "Erro ao enviar formulário! Os seguintes pedidos não foram enviados:";
 		}
 		$_SESSION['idEvento'] = NULL;
 	}
@@ -60,9 +62,9 @@
 		<div class="row">
 			<div class="col-md-offset-2 col-md-8">
 				<div class="section-heading">
-					<h3></h3> 
+					<h3></h3>
 					<p><?php //var_dump($_SESSION); ?></p>
-					<?php if(isset($mensagem)){echo $mensagem;} ?>					
+					<?php if(isset($mensagem)){echo $mensagem;} ?>
 					<div align="justify">
 						<strong>Número do Evento:</strong> <?php echo $evento['idEvento'] ?><br/>
 						<strong>Nome do Evento:</strong> <?php echo $evento['nomeEvento'] ?><br/>
@@ -78,6 +80,6 @@
 					<p><a href="?perfil=evento">Voltar ao módulo eventos?</a></p>
 				</div>
 			</div>
-		</div>	  
+		</div>
 	</div>
 </section>
