ff798082b2363db43dead40e6d39e69b33553db8
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-03-15 15:30:17 -0300

    Correção contratos - frm_busca

--- perfil/m_contratos/frm_busca.php
+++ perfil/m_contratos/frm_busca.php
@@ -1,177 +1,215 @@
 ﻿<?php include 'includes/menu.php';?>
 
 <?php
-if(isset($_GET['b']))
-{
-	$b = $_GET['b'];	
-}
-else
-{
-	$b = 'inicial';
-}
+if(isset($_GET['b'])):
+  $b = $_GET['b'];	
+else:
+  $b = 'inicial';
+endif;
 
 switch($b)
 {
-case 'inicial':
+  case 'inicial':
 
-if(isset($_POST['pesquisar']))
-{
-	$id = trim($_POST['id']);
-	$idEvento = trim($_POST['idEvento']);
-	$evento = trim($_POST['evento']);
-	$fiscal = $_POST['fiscal'];
-	$tipo = $_POST['tipo'];
-	$instituicao = $_POST['instituicao'];
-	$estado = $_POST['estado'];
-	$juridico = $_POST['juridico'];
-	$projeto = $_POST['projeto'];
-	if($id == "" AND $idEvento == "" AND $evento == "" AND $fiscal == 0 AND $tipo == 0 AND $instituicao == 0 AND $estado == 0 AND $juridico == 0 AND $projeto == 0)
-	{ 
-?>
-	<section id="services" class="home-section bg-white">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-offset-2 col-md-8">
-					<div class="section-heading">
-						<h2>Busca por pedido</h2>
-						<p>É preciso ao menos um critério de busca ou você pesquisou por um pedido inexistente. Tente novamente.</p>
-					</div>
-				</div>
+  /*CAPTURA DE DADOS DO FORMULARIO*/
+  if(isset($_POST['pesquisar']))
+  {
+    $id = trim($_POST['id']);
+    $idEvento = trim($_POST['idEvento']);
+    $evento = trim($_POST['evento']);
+    $fiscal = $_POST['fiscal'];
+    $tipo = $_POST['tipo'];
+    $instituicao = $_POST['instituicao'];
+    $estado = $_POST['estado'];
+    $juridico = $_POST['juridico'];
+    $projeto = $_POST['projeto'];
+  
+    /*VALIDA DE O POST TEM VALOR, CASO NÃO TENHA UM NOVO FORM É APRESENTADO*/
+    if($id == "" AND 
+      $idEvento == "" AND 
+      $evento == "" AND 
+      $fiscal == 0 AND 
+      $tipo == 0 AND 
+      $instituicao == 0 AND 
+      $estado == 0 AND 
+      $juridico == 0 AND 
+      $projeto == 0){ ?>
+	
+	 <!--FORM QUE É APRESENTADO CASO O POST ESTEJA VAZIO-->
+	 <section id="services" class="home-section bg-white">
+	   <div class="container">
+	     <div class="row">
+		   <div class="col-md-offset-2 col-md-8">
+		     <div class="section-heading">
+			   <h2>Busca por pedido</h2>
+				<p>É preciso ao menos um critério de busca ou você pesquisou por um pedido inexistente. 
+				   Tente novamente.
+				</p>
+			 </div>
 			</div>
-	        <div class="row">
-				<form method="POST" action="?perfil=contratos&p=frm_busca" class="form-horizontal" role="form">
-				<div class="form-group">
-					<div class="col-md-offset-2 col-md-8">
-						<h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
-						<label>Código do Pedido</label>
-							<input type="text" name="id" class="form-control" id="palavras" placeholder="Insira o Código do Pedido" ><br />
-						<label>ID do Evento</label>
-							<input type="text" name="idEvento" class="form-control" id="palavras" placeholder="Insira o Código do Evento" ><br />
-						<label>Nome do Evento</label>
-							<input type="text" name="evento" class="form-control" id="palavras" placeholder="Insira o objeto" ><br />
-						<label>Fiscal, suplente ou usuário que cadastrou o evento</label>
-							<select class="form-control" name="fiscal" id="inputSubject" >
-								<option value="0"></option>	
-								<?php echo opcaoUsuario($_SESSION['idInstituicao'],"") ?>
-							</select>
-						<br />
-						<label>Tipo de evento</label>
-							<select class="form-control" name="tipo" id="inputSubject" >
-								<option value="0"></option>		                
-								<?php echo geraOpcao("ig_tipo_evento","","") ?>
-							</select>	
-						<br />
-						<label>Instituição</label>
-							<select class="form-control" name="instituicao" id="inputSubject" >
-								<option value="0"></option>
-								<?php echo geraOpcao("ig_instituicao","","") ?>
-							</select>		
-						<br />
-						<label>Status do pedido</label>
-							<select class="form-control" name="estado" id="inputSubject" >
-								<option value='0'></option>
-								<?php echo geraOpcao("sis_estado","","") ?>
-							</select>		
-						<label>Tipo de Relação Jurídica</label>
-							<select class="form-control" name="juridico" id="inputSubject" >
-								<option value='0'></option>
-								<?php  geraOpcao("ig_modalidade","",""); ?>
-							</select>						
-						<label>Tipo de Projeto</label>
-							<select class="form-control" name="projeto" id="inputSubject" >
-								<option value='0'></option>
-								<?php  geraOpcao("ig_projeto_especial","",""); ?>
-						</select>
-					</div>
-				</div>
-				<br />             
-	            <div class="form-group">
-		            <div class="col-md-offset-2 col-md-8">
-						<input type="hidden" name="pesquisar" value="1" />
-						<input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">                    
-        	    	</div>					
-        	    </div>
-				</form>
-			</div>
-		</div>
-	</section>
-<?php
-	}
-	else
-	{
-		$con = bancoMysqli();
-		
-		if($id != "")
-		{ // Foi inserido o número do pedido
-			$sql_existe = "SELECT ped.idPedidoContratacao, ped.idEvento, ped.estado 
-						FROM igsis_pedido_contratacao AS ped 
-						INNER JOIN ig_evento AS eve ON eve.idEvento = ped.idEvento
-						WHERE idPedidoContratacao = '$id' AND ped.publicado = '1' AND eve.publicado = 1 AND eve.dataEnvio IS NOT NULL AND estado IS NOT NULL 
-						ORDER BY idPedidoContratacao DESC";
-			$query_existe = mysqli_query($con, $sql_existe);
-			$num_registro = mysqli_num_rows($query_existe);
+		 </div>
+	     <div class="row">
+		   <form method="POST" action="?perfil=contratos&p=frm_busca" class="form-horizontal" role="form">
+		     <div class="form-group">
+			   <div class="col-md-offset-2 col-md-8">
+			     <h5><?php if(isset($mensagem)){ echo $mensagem; } ?>
+				 <label>Código do Pedido</label>
+				 <input type="text" name="id" class="form-control" id="palavras" 
+				        placeholder="Insira o Código do Pedido" ><br />
+				 <label>ID do Evento</label>
+				 <input type="text" name="idEvento" class="form-control" id="palavras" 
+				        placeholder="Insira o Código do Evento" ><br />
+				 <label>Nome do Evento</label>
+				 <input type="text" name="evento" class="form-control" id="palavras" 
+				        placeholder="Insira o objeto" ><br />
+				 <label>Fiscal, suplente ou usuário que cadastrou o evento</label>
+				 <select class="form-control" name="fiscal" id="inputSubject" >
+				   <option value="0"></option>	
+				   <?php echo opcaoUsuario($_SESSION['idInstituicao'],"") ?>				    
+				 </select>
+				 <br />
+				 <label>Tipo de evento</label>
+				 <select class="form-control" name="tipo" id="inputSubject" >
+				   <option value="0"></option>		                
+				     <?php echo geraOpcao("ig_tipo_evento","","") ?>
+				 </select>	
+				 <br />
+				<label>Instituição</label>
+				<select class="form-control" name="instituicao" id="inputSubject" >
+				  <option value="0"></option>
+				  <?php echo geraOpcao("ig_instituicao","","") ?>
+				</select>		
+				<br />
+				<label>Status do pedido</label>
+				<select class="form-control" name="estado" id="inputSubject" >
+				  <option value='0'></option>
+				  <?php echo geraOpcao("sis_estado","","") ?>
+				</select>		
+				<label>Tipo de Relação Jurídica</label>
+				<select class="form-control" name="juridico" id="inputSubject" >
+				  <option value='0'></option>
+				  <?php  geraOpcao("ig_modalidade","",""); ?>
+				</select>						
+				<label>Tipo de Projeto</label>
+				<select class="form-control" name="projeto" id="inputSubject" >
+				  <option value='0'></option>
+				  <?php  geraOpcao("ig_projeto_especial","",""); ?>
+			    </select>
+			   </div>
+			 </div>
+			 <br />             
+	         <div class="form-group">
+		       <div class="col-md-offset-2 col-md-8">
+				  <input type="hidden" name="pesquisar" value="1" />
+				  <input type="submit" class="btn btn-theme btn-lg btn-block" value="Pesquisar">
+        	   </div>					
+        	 </div>
+		   </form>
+	     </div>
+	   </div>
+	 </section>
+    <?php
+	}else{
+	  
+	  $con = bancoMysqli();		
+	  //ENTRADA PARA BUSCA POR NUMERO DE PEDIDO*/
+	  if($id != ""){ 
+	    
+	    $sql_idPedido = 
+	      "SELECT 
+		    ped.idPedidoContratacao, 
+			ped.idEvento, 
+			ped.estado 
+			FROM 
+			  igsis_pedido_contratacao AS ped 
+			INNER JOIN 
+			  ig_evento AS eve 
+			ON eve.idEvento = ped.idEvento
+			AND  idPedidoContratacao = '$id' 
+			AND ped.publicado = '1' 
+			AND eve.publicado = 1 
+			AND eve.dataEnvio IS NOT NULL 
+			AND estado IS NOT NULL 
+			ORDER BY idPedidoContratacao DESC";			
+			
+			$query_idPedido = mysqli_query($con, $sql_idPedido);
+			$num_registro = mysqli_num_rows($query_idPedido);
 	
 			$pedido = recuperaDados("igsis_pedido_contratacao",$id,"idPedidoContratacao");
+
 			if($pedido['estado'] > 1)
 			{
-				$evento = recuperaDados("ig_evento",$pedido['idEvento'],"idEvento"); //$tabela,$idEvento,$campo
-				$projeto = recuperaDados("ig_projeto_especial",$pedido['idEvento'],"projetoEspecial");
-				$usuario = recuperaDados("ig_usuario",$evento['idUsuario'],"idUsuario");
-				$instituicao = recuperaDados("ig_instituicao",$evento['idInstituicao'],"idInstituicao");
-				$local = listaLocais($pedido['idEvento']);
-				$periodo = retornaPeriodo($pedido['idEvento']);
-				$duracao = retornaDuracao($pedido['idEvento']);
-				$pessoa = recuperaPessoa($pedido['idPessoa'],$pedido['tipoPessoa']);
-				$fiscal = recuperaUsuario($evento['idResponsavel']);
-				$suplente = recuperaUsuario($evento['suplente']);
-				$protocolo = ""; //recuperaDados("sis_protocolo",$pedido['idEvento'],"idEvento");
-				$operador = recuperaUsuario($pedido['idContratos']);
-				if($pedido['parcelas'] > 1)
-				{
-					$valorTotal = somaParcela($pedido['idPedidoContratacao'],$pedido['parcelas']);
+			  if($num_registro == 0)
+			  {
+			    $x['num'] = 0;
+			  }
+			  else
+			  {
+			    $i = 0;
+				while($lista = mysqli_fetch_array($query_idPedido))
+				{			
+				  $pedido = recuperaDados("igsis_pedido_contratacao",
+				  	                       $lista['idPedidoContratacao'],
+				  	                       "idPedidoContratacao");
+				
+				  $evento = recuperaDados("ig_evento",$pedido['idEvento'],"idEvento"); 
+				  $usuario = recuperaDados("ig_usuario",$evento['idUsuario'],"idUsuario");
+				  $instituicao = recuperaDados("ig_instituicao",$evento['idInstituicao'],"idInstituicao");
+				  $local = listaLocais($pedido['idEvento']);
+				  $periodo = retornaPeriodo($pedido['idEvento']);
+				  $pessoa = recuperaPessoa($pedido['idPessoa'],$pedido['tipoPessoa']);
+				  $fiscal = recuperaUsuario($evento['idResponsavel']);
+				  $suplente = recuperaUsuario($evento['suplente']);
+				  $operador = recuperaUsuario($pedido['idContratos']);
+				  if($pedido['parcelas'] > 1)
+				  {
+				    $valorTotal = somaParcela($pedido['idPedidoContratacao'],$pedido['parcelas']);
 					$formaPagamento = txtParcelas($pedido['idPedidoContratacao'],$pedido['parcelas']);	
-				}
-				else
-				{
-					$valorTotal = $pedido['valor'];
+				  }
+				  else
+				  {
+				    $valorTotal = $pedido['valor'];
 					$formaPagamento = $pedido['formaPagamento'];
-				}		
-				$x[0]['id']= $pedido['idPedidoContratacao'];
-				$x[0]['NumeroProcesso']= $pedido['NumeroProcesso'];
-				$x[0]['objeto'] = retornaTipo($evento['ig_tipo_evento_idTipoEvento'])." - ".$evento['nomeEvento'];
-				switch($pedido['tipoPessoa'])
-				{
-					case 1:
-						$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
-						$x[0]['proponente'] = $pessoa['Nome'];
-						$x[0]['tipo'] = "Física";
-						$x[0]['pessoa'] = 1;
-					break;
-					case 2:
-						$pessoa = recuperaDados("sis_pessoa_juridica",$pedido['idPessoa'],"Id_PessoaJuridica");
-						$x[0]['proponente'] = $pessoa['RazaoSocial'];
-						$x[0]['tipo'] = "Jurídica";
-						$x[0]['pessoa'] = 2;
-					break;
-					case 4:
-						$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
-						$x[0]['proponente'] = $pessoa['Nome'];
-						$x[0]['tipo'] = "Formação";
-						$x[0]['pessoa'] = 4;
-					break;
-				}
-				$x[0]['valor'] = $pedido['valor'];
-				$x[0]['local'] = substr($local,1);
-				$x[0]['instituicao'] = $instituicao['sigla'];
-				$x[0]['periodo'] = $periodo;
-				$x[0]['pendencia'] = $pedido['pendenciaDocumento'];
-				$x[0]['status'] = $pedido['estado'];
-				$x[0]['operador'] = $operador['nomeCompleto'];	
-				$x['num'] = 1;
-			}
+				  }		
+					$x[$i]['id']= $pedido['idPedidoContratacao'];
+					$x[$i]['NumeroProcesso']= $pedido['NumeroProcesso'];
+					$x[$i]['objeto'] = retornaTipo($evento['ig_tipo_evento_idTipoEvento'])." - ".$evento['nomeEvento'];
+					switch($pedido['tipoPessoa'])
+					{
+						case 1:
+							$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
+							$x[$i]['proponente'] = $pessoa['Nome'];
+							$x[$i]['tipo'] = "Física";
+							$x[$i]['pessoa'] = 1;
+						break;
+						case 2:
+							$pessoa = recuperaDados("sis_pessoa_juridica",$pedido['idPessoa'],"Id_PessoaJuridica");
+							$x[$i]['proponente'] = $pessoa['RazaoSocial'];
+							$x[$i]['tipo'] = "Jurídica";
+							$x[$i]['pessoa'] = 2;
+						break;
+						case 4:
+							$pessoa = recuperaDados("sis_pessoa_fisica",$pedido['idPessoa'],"Id_PessoaFisica");
+							$x[$i]['proponente'] = $pessoa['Nome'];
+							$x[$i]['tipo'] = "Formação";
+							$x[$i]['pessoa'] = 4;
+						break;
+					}
+					$x[$i]['valor'] = $pedido['valor'];
+					$x[$i]['local'] = substr($local,1);
+					$x[$i]['instituicao'] = $instituicao['sigla'];
+					$x[$i]['periodo'] = $periodo;
+					$x[$i]['pendencia'] = $pedido['pendenciaDocumento'];
+					$x[$i]['status'] = $pedido['estado'];
+					$x[$i]['operador'] = $operador['nomeCompleto'];
+					$i++;	
+				}//end while	
+				$x['num'] = $i;
+			}//endelse						
+			}//endif
 			else
 			{
-				$x['num'] = 0;
+			  $x['num'] = 0;
 			}
 		}
 		elseif($idEvento != "")
@@ -247,9 +285,9 @@ if(isset($_POST['pesquisar']))
 					$x[$i]['status'] = $pedido['estado'];
 					$x[$i]['operador'] = $operador['nomeCompleto'];
 					$i++;	
-				}	
+				}//end while	
 				$x['num'] = $i;
-			}		
+			}//endelse		
 		}
 		else
 		{ //Não foi inserido o número do pedido
