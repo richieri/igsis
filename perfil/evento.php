6814d076f55c2a65e7321123700029c8b75b1f7f
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-04-20 17:35:05 -0300

    virada 2017 > virada 2018

--- perfil/evento.php
+++ perfil/evento.php
@@ -2149,7 +2149,7 @@
 					</div>
 					<div class="form-group">         			
 						<div class="col-md-offset-2 col-md-8">
-							<input type="checkbox" name="virada" id="virada" onclick="habilitar()" <?php checar($ocor['virada']) ?>/><label  style="padding:0 20px 0 5px;">Virada 2017</label>
+							<input type="checkbox" name="virada" id="virada" onclick="habilitar()" <?php checar($ocor['virada']) ?>/><label  style="padding:0 20px 0 5px;">Virada 2018</label>
 						</div>                     
 					</div>
 					<div class="form-group">
