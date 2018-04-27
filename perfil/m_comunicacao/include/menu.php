61bed302f7afba5fb61823b2ee728bc7ca31f151
LoreleiGab <loreleigab@hotmail.com>
2018-04-27 14:43:44 -0300

    Lista com os eventos da Virada no módulo Comunicação

--- perfil/m_comunicacao/include/menu.php
+++ perfil/m_comunicacao/include/menu.php
@@ -11,17 +11,18 @@ $link = $http."rlt_comunicacao_fotos.php";
 <div class="menu-area">
 	<div id="dl-menu" class="dl-menuwrapper">
 		<button class="dl-trigger">Busca</button>
-			<ul class="dl-menu">				
+			<ul class="dl-menu">
 				<li><a href="<?php echo $pasta ?>filtro">Filtrar</a></li>
 				<li><a href="<?php echo $pasta ?>programacao_local">Programação Local</a></li>
 				<li><a href='<?php echo $link ?>' target='_blank'>Relatório de Foto do Mês</a></li>
-				<li><a href="<?php echo $pasta ?>docs">Em Cartaz</a></li>   
-				<!--<li><a href="<?php echo $pasta ?>programacao_local">Agenda</a></li>-->				
-				<li><a href="<?php echo $pasta ?>chamados">Lista de chamados</a></li> 			
+				<li><a href="<?php echo $pasta ?>docs">Em Cartaz</a></li>
+				<li><a href="<?php echo $pasta ?>lista_virada">Virada 2018</a></li>
+				<!--<li><a href="<?php echo $pasta ?>programacao_local">Agenda</a></li>-->
+				<li><a href="<?php echo $pasta ?>chamados">Lista de chamados</a></li>
 				<li style="color:white;">-------------------------</li>
 				<li><a href="?secao=perfil">Carregar módulo</a></li>
 				<li selected><a href="http://smcsistemas.prefeitura.sp.gov.br/igsis/manual/index.php/modulo-comunicacao/">Ajuda</a></li>
 				<li><a href="../include/logoff.php">Sair</a></li>
 			</ul>
 	</div>
-</div>	
+</div>
---
+++ perfil/m_comunicacao/lista_virada.php
@@ -0,0 +1,96 @@
+<?php
+include 'include/menu.php';
+$con = bancoMysqli();
+
+//verifica a página atual caso seja informada na URL, senão atribui como 1ª página
+$pagina = (isset($_GET['pagina']))? $_GET['pagina'] : 1;
+$idInsituicao = $_SESSION['idInstituicao'];
+$sql_lista = "
+	SELECT eve.idEvento, eve.nomeEvento, eve.nomeGrupo, loc.sala, age.data, age.hora, tip.tipoEvento, eve.sinopse
+	FROM igsis_agenda AS age
+	INNER JOIN ig_evento AS eve ON eve.idEvento = age.idEvento
+	INNER JOIN ig_local AS loc ON loc.idLocal = age.idLocal
+	INNER JOIN ig_tipo_evento AS tip ON tip.idTipoEvento = age.idTipo
+	WHERE eve.publicado=1 AND eve.dataEnvio IS NOT NULL AND eve.projetoEspecial IN (54) AND age.data BETWEEN '2018-05-19' AND '2018-05-20'
+	ORDER BY eve.nomeEvento, age.data, age.hora";
+$query_lista = mysqli_query($con,$sql_lista);
+
+//conta o total de itens
+$total_geral = mysqli_num_rows($query_lista);
+
+//seta a quantidade de itens por página
+$registros = 100;
+
+//calcula o número de páginas arredondando o resultado para cima
+$numPaginas = ceil($total_geral/$registros);
+
+//variavel para calcular o início da visualização com base na página atual
+$inicio = ($registros*$pagina)-$registros;
+
+//seleciona os itens por página
+$sql_lista = "
+	SELECT eve.idEvento, eve.nomeEvento, eve.nomeGrupo, loc.sala, age.data, age.hora, tip.tipoEvento, eve.sinopse
+	FROM igsis_agenda AS age
+	INNER JOIN ig_evento AS eve ON eve.idEvento = age.idEvento
+	INNER JOIN ig_local AS loc ON loc.idLocal = age.idLocal
+	INNER JOIN ig_tipo_evento AS tip ON tip.idTipoEvento = age.idTipo
+	WHERE eve.publicado=1 AND eve.dataEnvio IS NOT NULL AND eve.projetoEspecial IN (54) AND age.data BETWEEN '2018-05-19' AND '2018-05-20'
+	ORDER BY eve.nomeEvento, age.data, age.hora
+	LIMIT $inicio,$registros";
+$query_lista = mysqli_query($con,$sql_lista);
+
+//conta o total de itens
+$total = mysqli_num_rows($query_lista);
+?>
+<section id="list_items" class="home-section bg-white">
+	<div class="container">
+		<div class="sub-title"><h4>EVENTOS VIRADA CULTURAL 2018</h4>
+		</div>
+		<p><strong>Total de registros:</strong> <?php echo $total_geral;?> | <strong>Registros nesta página:</strong> <?php echo $total;?></p>
+		<div class="table-responsive list_info">
+			<table class="table table-condensed">
+				<thead>
+					<tr class="list_menu">
+						<td>Codigo do evento</td>
+						<td>Nome do evento</td>
+						<td>Nome do grupo</td>
+						<td>Local</td>
+						<td>Data apresentação</td>
+						<td>Horário inicial</td>
+						<td>Categoria</td>
+						<td>Descrição</td>
+					</tr>
+				</thead>
+				<tbody>
+			<?php
+				while($campo = mysqli_fetch_array($query_lista))
+				{
+					echo "<tr>";
+					echo "<td class='list_description'>".$campo['idEvento']."</td>";
+					echo "<td class='list_description'>".$campo['nomeEvento']."</td>";
+					echo "<td class='list_description'>".$campo['nomeGrupo']."</td>";
+					echo "<td class='list_description'>".$campo['sala']."</td>";
+					echo "<td class='list_description'>".exibirDataBr($campo['data'])."</td>";
+					echo "<td class='list_description'>".$campo['hora']."</td>";
+					echo "<td class='list_description'>".$campo['tipoEvento']."</td>";
+					echo "<td class='list_description'>".$campo['sinopse']."</td>";
+					echo "</tr>";
+				}
+			?>
+					<tr>
+						<td colspan="10" bgcolor="#DEDEDE">
+						<?php
+							//exibe a paginação
+							echo "<strong>Páginas</strong>";
+							for($i = 1; $i < $numPaginas + 1; $i++)
+							{
+								echo "<a href='?perfil=comunicacao&p=lista_virada&pagina=$i'> [".$i."]</a> ";
+							}
+						?>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</section>
