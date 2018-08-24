9bbe109ed9e69b4088198dcf4f43192d4656e80a
Grayce Ferreira <grayce.ferreira@gmail.com>
2018-08-24 12:35:30 -0300

    Impressão docs - Contratos Lite

--- perfil/m_contratos_lite/impressao_contratos_pf.php
+++ perfil/m_contratos_lite/impressao_contratos_pf.php
@@ -19,6 +19,7 @@ $link04 = $http."rlt_proposta_formacao.php"."?id=".$id_ped."&penal";
 $link05 = $http."rlt_proposta_exposicao_edital_word_pf.php"."?id=".$id_ped."&penal";
 $link06 = $http."rlt_proposta_emia.php"."?id=".$id_ped."&penal";
 $link07 = $http."rlt_proposta_pesquisador_formacao.php"."?id=".$id_ped;
+$link47 = $http."rlt_proposta_reversao_pf.php"."?id=".$id_ped."&penal";
 
 $link18 = $http."rlt_declaracao_iss_pf.php";
 $link19 = $http."rlt_declaracao_naoservidor_pf.php";
@@ -32,13 +33,16 @@ $link25 = $http."rlt_pedido_reserva_vocacional_pf.php";
 $link29 = $http."rlt_pedido_reserva_centrosculturais_pf.php";
 $link30 = $http."rlt_pedido_reserva_gabinete_pf.php";
 $link31 = $http."rlt_pedido_reserva_dph_pf.php";
-$link32 = $http."rlt_pedido_reserva_csmb_pf.php";
+$link32 = $http."rlt_pedido_reserva_csmb
+_pf.php";
 $link33 = $http."rlt_pedido_reserva_casasdecultura_pf.php";
 $link41 = $http."rlt_pedido_reserva_ccsp_pf.php";
 $link42 = $http."rlt_pedido_reserva_oficina.php";
 $link43 = $http."rlt_pedido_reserva_bma_pf.php";
 $link44 = $http."rlt_reserva_pf.php";
 $link45 = $http."rlt_pedido_reserva_pia_pf.php";
+$link46 = $http."rlt_pedido_reserva_vocacional_sme_pf.php";
+
 
 
 $link17 = $http."rlt_normas_internas_teatros_pf.php"."?id=".$id_ped;
@@ -76,43 +80,20 @@ echo "
 
 						<tr class='list_menu'><td colspan='2'><strong>PROPOSTA</strong></td></tr>
 
-						<tr>
-							<td><a href='$link03=3' target='_blank'><strong>Cinema</strong></a></td>
-							<td><a href='$link03=5' target='_blank'><strong>Comissão Julgadora</strong></a></td>
-						</tr>
-
-						<tr>
-							<td><a href='$link03=6' target='_blank'><strong>Doação de Serviços</strong></a></td>
-							<td><a href='$link03=12' target='_blank'><strong>Oficinas sem Edital</strong></a></td>
-						</tr>
-
-						<tr>
-							<td><a href='$link03=16' target='_blank'><strong>Curta Temporada</strong></a></td>
-							<td><a href='$link03=18' target='_blank'><strong>Longa Temporada</strong></a></td>
-						</tr>
-
+				
 						<tr>
 							<td><a href='$link04=20' target='_blank'><strong>Vocacional</strong></a></td>
 							<td><a href='$link04=21' target='_blank'><strong>PIÁ</strong></a></td>
 						</tr>
 
 						<tr>
-							<td><a href='$link05=23' target='_blank'><strong>Edital - Exposição</strong></a></td>
-							<td><a href='$link03=25' target='_blank'><strong>Edital - Mediação</strong></a></td>
-						</tr>
-
-						<tr>
-							<td><a href='$link03=26' target='_blank'><strong>Edital - Contação de Histórias</strong></a></td>
-							<td><a href='$link03=29' target='_blank'><strong>Edital - Gala de Balé</strong></a></td>
-						</tr>
-
-						<tr>
+							<td><a href='$link05=23' target='_blank'><strong>Editais</strong></a></td>
 							<td><a href='$link06=31' target='_blank'><strong>EMIA</strong></a></td>
-							<td><a href='$link07' target='_blank'><strong>Pesquisador Formação</strong></a></td>
 						</tr>
 
 						<tr>
-							<td colspan='2'><a href='$link03=15' target='_blank'><strong>Palestra | Debate | Workshop | Exposição | Outros</strong></a></td>
+							<td><a href='$link03=13' target='_blank'><strong>Contratações gerais - Com cachê</strong></a></td>
+							<td><a href='$link47=13' target='_blank'><strong>Contratações gerais - Reversão de Bilheteria</strong></a></td>
 						</tr>
 
 						<tr><td class='list_description'><br/></td></tr>
@@ -120,16 +101,11 @@ echo "
 						<tr class='list_menu'><td colspan='2'><strong>DECLARAÇÃO</strong></td></tr>
 
 						<tr>
-							<td><a href='$link19?id=$id_ped' target='_blank'><strong>Não Servidor</strong></a></td>
-							<td><a href='$link18?id=$id_ped' target='_blank'><strong>ISS</strong></a></td>
-						</tr>
-						<tr>
 							<td><a href='$link23?id=$id_ped' target='_blank'><strong>Direitos Conexos</strong></a></td>
 							<td><a href='$link20?id=$id_ped' target='_blank'><strong>Exclusividade</strong></a></td>
 						</tr>
 						<tr>
 							<td><a href='$link21?id=$id_ped' target='_blank'><strong>Convênio 500</strong></a></td>
-							<td><a href='$link22?id=$id_ped' target='_blank'><strong>Abertura de Conta - Banco do Brasil </strong></a></td>
 						</tr>
 
 						<tr><td class='list_description'><br/></td></tr>
@@ -140,19 +116,8 @@ echo "
 							<td><a href='$link34' target='_blank'><strong>FACC</strong></a></td>
 							<td><a href='$link37?id=$id_ped' target='_blank'><strong>Parecer da Comissão</strong></a></td>
 						</tr>
-						<tr>
-							<td><a href='$link35?id=$id_ped' target='_blank'><strong>Ordem de serviço - CCSP Assinatura</strong></a></td>
-							<td><a href='$link36?id=$id_ped' target='_blank'><strong>Ordem de serviço - Gabinete Assinatura</strong></a></td>
-						</tr>
 
 						<tr>
-							<td><a href='$link40?id=$id_ped' target='_blank'><strong>Termo Doação de Serviço - Assinatura</strong></a></td>
-							<td><a href='$link39?id=$id_ped' target='_blank'><strong>Termo de Parceria</strong></a></td>
-
-						</tr>
-
-						<tr>
-							<td><a href='$link38?id=$id_ped' target='_blank'><strong>Termo de Doação de Obra de Arte</strong></a></td>
 							<td><a href='$link17' target='_blank'><strong>NORMAS INTERNAS - Teatros Municipais</strong></a></td>
 						</tr>
 
@@ -164,11 +129,13 @@ echo "
 							<td><a href='$link25?id=$id_ped' target='_blank'><strong>FORMAÇÃO - Vocacional</strong></a></td>
 							<td><a href='$link45?id=$id_ped' target='_blank'><strong>FORMAÇÃO - PIÁ</strong></a></td>
 						</tr>
+
+						<tr>
+							<td><a href='$link46?id=$id_ped' target='_blank'><strong>VOCACIONAL/PIÁ - SME</strong></a></td>
 							<td><a href='$link29?id=$id_ped' target='_blank'><strong>Reserva Centros Culturais</strong></a></td>
-							<td><a href='$link31?id=$id_ped' target='_blank'><strong>Reserva DPH</strong></a></td>
 						</tr>
 
-						</tr>
+						<tr>
 							<td><a href='$link32?id=$id_ped' target='_blank'><strong>Reserva CSMB</strong></a></td>
 							<td><a href='$link33?id=$id_ped' target='_blank'><strong>Reserva Casas de Cultura</strong></a></td>
 						</tr>
@@ -186,8 +153,12 @@ echo "
 						<tr>
 							<td><a href='$link44?id=$id_ped' target='_blank'><strong>Reserva Pessoa Física</strong></a></td>
 							<td><a href='$link30?id=$id_ped' target='_blank'><strong>Reserva Gabinete</strong></a></td>
-
 						</tr>
+
+						<tr>
+							<td><a href='$link31?id=$id_ped' target='_blank'><strong>Reserva DPH</strong></a></td>
+						</tr>
+
 
 					</tbody>
 				</table>
