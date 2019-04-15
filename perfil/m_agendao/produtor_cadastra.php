ca63ed19cfd20a14ecc4763b386b72fae475f4b6
Lorelei Gabriele <loreleigab@hotmail.com>
2019-04-15 12:18:36 -0300

    Cadastro, edição e botões de avaçar e voltar em produtor do agendão

--- perfil/m_agendao/produtor_cadastra.php
+++ perfil/m_agendao/produtor_cadastra.php
@@ -1,29 +1,53 @@
 <?php
+$idEvento = (isset($_POST['idEvento'])) ? $_POST['idEvento'] : null;
 
-if (isset($_POST['cadastra'])) {
+$con = bancoMysqli();
 
+if(isset($_POST['cadastra']) || isset($_POST['atualiza'])){
     $nome = $_POST['produtorNome'];
     $telefone1 = $_POST['ig_produtor_telefone'];
     $email = $_POST['email'];
     $telefone2 = $_POST['telefone2'] ?? null;
+}
 
+if (isset($_POST['cadastra'])) {
 
-    $con = bancoMysqli();
     $sql = "INSERT INTO ig_produtor (nome, email, telefone, telefone2)  VALUES ('$nome', '$email', '$telefone1', '$telefone2' )";
 
     if (mysqli_query($con, $sql))
     {
         $idProdutor = recuperaUltimo('ig_produtor');
-        $mensagem =  "Cadastrado com sucesso!";
-        gravarLog($sql);
-    } else {
+        $sql_evento = "UPDATE ig_evento SET ig_produtor_idProdutor = '$idProdutor' WHERE idEvento = '$idEvento'";
+        if($con->query($sql_evento)){
+            $mensagem =  "Cadastrado com sucesso!";
+            gravarLog($sql);
+        }
+        else{
+            $mensagem = "Erro ao gravar! Tente novamente.";
+        }
+    }
+    else {
         $mensagem = "Erro ao gravar! Tente novamente.";
         gravarLog($sql);
     }
-
-    $produtor = recuperaDados("ig_produtor","$idProdutor","idProdutor");
-
 }
+
+if(isset($_POST['atualiza'])){
+    $idProdutor = $_POST['idProdutor'];
+    $sql = "UPDATE ig_produtor SET nome = '$nome', email = '$email', telefone = '$telefone1', telefone2 = '$telefone2' WHERE idProdutor = '$idProdutor'";
+    if ($con->query($sql)){
+        $mensagem =  "Atualizado com sucesso!";
+        gravarLog($sql);
+    }
+    else{
+        $mensagem = "Erro ao atualizar! Tente novamente.";
+    }
+}
+
+$evento = $con->query("SELECT ig_produtor_idProdutor FROM ig_evento WHERE idEvento = '$idEvento'")-> fetch_assoc();
+
+$idProdutor = $evento['ig_produtor_idProdutor'];
+$produtor = recuperaDados("ig_produtor",$idProdutor,"idProdutor");
 
 include "include/menu.php";
 ?>
@@ -65,12 +89,28 @@ include "include/menu.php";
 
                     <div class="form-group">
                         <div class="col-md-offset-2 col-md-8">
-                            <input type="submit" class="btn btn-theme btn-lg btn-block" name="cadastra" value="Gravar">
+                            <input type="hidden" name="idEvento" value="<?=$idEvento?>">
+                            <input type="hidden" name="idProdutor" value="<?=$idProdutor?>">
+                            <input type="submit" class="btn btn-theme btn-lg btn-block" name="<?=($idProdutor == 0) ? "cadastra" : "atualiza"?>" value="Gravar">
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-        <!-- TODO: Incluir um botão de avançar após o usuário gravar -->
+        <hr>
+        <div class="row col-md-offset-1 col-md-10">
+            <div class="col-md-2 pull-left">
+                <form method="POST" action="?perfil=agendao&p=evento_cadastra" class="form-horizontal" role="form">
+                    <input type="hidden" name="idEvento" value="<?=$idEvento?>">
+                    <input type="submit" class="btn btn-theme btn-lg btn-block" value="Voltar">
+                </form>
+            </div>
+            <div class="col-md-2 pull-right">
+                <form method="POST" action="?perfil=agendao&p=ocorrencia_cadastra" class="form-horizontal" role="form">
+                    <input type="hidden" name="idEvento" value="<?=$idEvento?>">
+                    <input type="submit" class="btn btn-theme btn-lg btn-block" value="Avançar">
+                </form>
+            </div>
+        </div>
     </div>
 </section>
