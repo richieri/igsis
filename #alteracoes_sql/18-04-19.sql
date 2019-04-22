9228973e5ca2793c02c61d7cfc7845e29b45eefe
Lorelei Gabriele <loreleigab@hotmail.com>
2019-04-22 15:03:28 -0300

    Mudança no banco - Acréscimo de subprefeitura

--- #alteracoes_sql/18-04-19.sql
+++ #alteracoes_sql/18-04-19.sql
@@ -68,3 +68,48 @@ ALTER TABLE `ig_papelusuario` ADD `agendao` TINYINT(1) NULL DEFAULT NULL AFTER `
 UPDATE `ig_papelusuario` SET `agendao` = '1' WHERE `ig_papelusuario`.`idPapelUsuario` = 1;
 
 ALTER TABLE `ig_evento` ADD `numero_apresentacao` INT NOT NULL DEFAULT '0' AFTER `statusEvento`, ADD `espaco_publico` TINYINT(1) NOT NULL DEFAULT '0' AFTER `numero_apresentacao`;
+
+-- 22/04/2019 -> Lorelei
+CREATE TABLE `igsis_subprefeitura` (`id` tinyint(2) NOT NULL, `subprefeitura` varchar(30) DEFAULT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `igsis_subprefeitura` (`id`, `subprefeitura`) VALUES
+(1, 'Aricanduva/Vila Formosa'),
+(2, 'Butantã'),
+(3, 'Campo Limpo'),
+(4, 'Capela do Socorro'),
+(5, 'Casa Verde / Cachoeirinha'),
+(6, 'Cidade Ademar'),
+(7, 'Cidade Tiradentes'),
+(8, 'Ermelino Matarazzo'),
+(9, 'Freguesia do Ó / Brasilândia'),
+(10, 'Guaianases'),
+(11, 'Ipiranga'),
+(12, 'Itaim Paulista'),
+(13, 'Itaquera'),
+(14, 'Jabaquara'),
+(15, 'Jaçanã/Tremembé'),
+(16, 'Lapa'),
+(17, 'M\'Boi Mirim'),
+(18, 'Mooca'),
+(19, 'Parelheiros'),
+(20, 'Penha'),
+(21, 'Perus'),
+(22, 'Pinheiros'),
+(23, 'Pirituba/Jaraguá'),
+(24, 'Santana/Tucuruvi'),
+(25, 'Santo Amaro'),
+(26, 'São Mateus'),
+(27, 'São Miguel Paulista'),
+(28, 'Sapopemba'),
+(29, 'Sé'),
+(30, 'Vila Maria / Vila Guilherme'),
+(31, 'Vila Mariana'),
+(32, 'Vila Prudente');
+
+ALTER TABLE `igsis_subprefeitura`
+   ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `igsis_subprefeitura`
+   MODIFY `id` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+ALTER TABLE `ig_ocorrencia` ADD `idSubprefeitura` TINYINT(2) NULL AFTER `observacao`;
