fd97eca4cf5d7061008d1bd9d84d5668cf655be3
Lorelei Gabriele <loreleigab@hotmail.com>
2019-11-14 13:04:45 -0300

    Inclusão do 3º parágrafo na proposta padrão

--- pdf/rlt_proposta_padrao_pj.php
+++ pdf/rlt_proposta_padrao_pj.php
@@ -425,8 +425,6 @@ $l=5; //DEFINE A ALTURA DA LINHA
    $pdf->SetFont('Arial','B', 9);
    $pdf->Cell(160,5,utf8_decode('NOS CASOS DE REVERSÃO DE BILHETERIA'),0,1,'C');
 
-   $pdf->Ln();
-
    $pdf->SetX($x);
    $pdf->SetFont('Arial','', 9);
    $pdf->MultiCell(180,5,utf8_decode('1) No caso de pagamento do cachê por reversão de bilheteria, fica o valor dos ingressos sujeito ao atendimento no disposto nas Leis Municipais nº 10.973/91, regulamentada pelo Decreto Municipal nº 30.730/91; Leis Municipais 11.113/91; 11.357/93 e 12.975/2000 e Portaria nº 66/SMC/2007; Lei Estadual nº 7844/92, regulamentada pelo Decreto Estadual nº 35.606/92; Lei Estadual nº 10.858/2001, com as alterações da Lei Estadual 14.729/2012 e Lei Federal nº 12.933/2013.'));
@@ -435,6 +433,10 @@ $l=5; //DEFINE A ALTURA DA LINHA
    $pdf->SetFont('Arial','', 9);
    $pdf->MultiCell(180,5,utf8_decode('2) O pagamento do cachê corresponderá à reversão integral da renda obtida na bilheteria a/o ontratada/o, deduzidos os impostos e taxas pertinentes.'));
 
+	$pdf->SetX($x);
+	$pdf->SetFont('Arial','', 9);
+	$pdf->MultiCell(180,5,utf8_decode('3) Os ingressos poderão ser vendidos com preços reduzidos, em face de promoções realizadas pela produção do evento.'));
+
    $pdf->Ln();
 
    $pdf->SetX($x);
@@ -442,8 +444,6 @@ $l=5; //DEFINE A ALTURA DA LINHA
    $pdf->Cell(10,$l,'',0,0,'L');
    $pdf->SetFont('Arial','B', 9);
    $pdf->Cell(160,5,utf8_decode('RESCISÃO'),0,1,'C');
-
-   $pdf->Ln();
 
    $pdf->SetX($x);
    $pdf->SetFont('Arial','', 9);
@@ -456,8 +456,6 @@ $l=5; //DEFINE A ALTURA DA LINHA
    $pdf->Cell(10,$l,'',0,0,'L');
    $pdf->SetFont('Arial','B', 9);
    $pdf->Cell(160,5,utf8_decode('FORO'),0,1,'C');
-
-   $pdf->Ln();
 
    $pdf->SetX($x);
    $pdf->SetFont('Arial','', 9);
