83df3f7a44b7d45cd8efdcd04d76d3efc3c4b50e
Diego Vicente <diego.vdossantos@gmail.com>
2020-02-14 12:27:18 -0300

    relatório dos proponentes por evento

--- pdf/relatorio_espacos_abertos.php
+++ pdf/relatorio_espacos_abertos.php
@@ -3,14 +3,28 @@
 require_once("../funcoes/funcoesConecta.php");
 require_once("../funcoes/funcoesGerais.php");
 require_once("../funcoes/funcoesSiscontrat.php");
+$con = bancoMysqli();
 
 $dataAtual = date('Y:m:d H:i:s');
+
+$sql = "SELECT
+            ped.idPedidoContratacao,
+            ie.idEvento,
+            ie.nomeEvento,
+            ipe.projetoEspecial,
+            ped.tipoPessoa,
+            ped.idPessoa
+        FROM igsis.igsis_pedido_contratacao AS ped
+        INNER JOIN igsis.ig_evento AS ie on ped.idEvento = ie.idEvento
+        LEFT JOIN igsis.ig_projeto_especial AS ipe on ie.projetoEspecial = ipe.idProjetoEspecial
+        WHERE ped.publicado = 1
+        ORDER BY ie.idEvento";
+$query = $con->query($sql)->fetch_all(MYSQLI_ASSOC);
 
 header ("Pragma: no-cache");
 header ("Content-type: application/x-msexcel");
 header ("Content-Disposition: attachment; filename=$dataAtual proponentes_espacos_abertos.xls" );
 
-$con = bancoMysqli();
 ?>
 <html>
 <meta http-equiv=\"Content-Type\" content=\"text/html; charset=Windows-1252\">
@@ -19,34 +33,18 @@ $con = bancoMysqli();
     <thead>
     <tr class="list_menu">
         <td>Nome do Evento</td>
-        <td>Local</td>
+        <td>Projeto Especial</td>
         <td>Proponente</td>
         <td>E-mail</td>
     </tr>
     </thead>
     <tbody>
     <?php
-    $sql = "SELECT
-                    eve.idEvento,
-                    ped.idPedidoContratacao,
-                    eve.nomeEvento,
-                    ped.tipoPessoa,
-                    ped.idPessoa,
-                    loc.sala
-                FROM igsis.igsis_pedido_contratacao AS ped
-                INNER JOIN igsis.ig_ocorrencia AS oco ON ped.idEvento = oco.idEvento
-                INNER JOIN igsis.ig_evento AS eve ON eve.idEvento = ped.idEvento
-                INNER JOIN igsis.ig_local AS loc ON loc.idLocal = oco.local
-                WHERE oco.local IN (SELECT idLocal FROM igsis.ig_local WHERE idInstituicao = 18)
-                    AND ped.publicado = 1
-                    AND oco.publicado = 1
-                GROUP BY ped.idEvento";
-    $query = $con->query($sql)->fetch_all(MYSQLI_ASSOC);
     foreach ($query as $dados) {
         ?>
         <tr>
             <td><?=$dados['nomeEvento']?></td>
-            <td><?=$dados['sala']?></td>
+            <td><?=$dados['projetoEspecial']?></td>
             <?php
             if ($dados['tipoPessoa'] == 1 ) {
                 $proponente = recuperaDados('sis_pessoa_fisica', $dados['idPessoa'], 'Id_PessoaFisica');
