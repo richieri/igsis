c391662af6ae9a50622cb3d5abf4eb3e3f2584c7
LoreleiGab <loreleigab@hotmail.com>
2020-03-30 20:32:03 -0300

    proposta formação

--- pdf/rlt_proposta_formacao.php
+++ pdf/rlt_proposta_formacao.php
@@ -372,9 +372,23 @@ $l=5; //DEFINE A ALTURA DA LINHA
    	   $pdf->SetX($x);
    	   $pdf->SetFont('Arial','', 10);
    	   $pdf->MultiCell(180,$l,utf8_decode("De $inicio a $fim - até $horas horas"));
-   	}
-	}
+      }
+   }
 	//fim cronograma
+
+$pdf->Ln();
+
+$pdf->SetX($x);
+$pdf->SetFont('Arial','B', 10);
+$pdf->Cell(22,$l,'Regime emergencial:',0,1,'L');
+$pdf->SetFont('Arial','', 10);
+$pdf->MultiCell(180,$l,utf8_decode("
+Durante o período em que os equipamentos Culturais e da Educação estiverem fechados, é do intuito da SMC tornar pública e acessível a formação a partir de suas diversas linguagens artísticas, pesquisas, mantendo como destinatário o publico-alvo do Programa.
+Será da atribuição dos Artistas Educadores/Orientadores:
+- A criação de materiais artístico pedagógicos em diversos suportes: vídeo, imagem, som e texto; que podem ser conteúdos ao vivo ou gravados, na forma de aulas, vivências, ações artísticas alinhados aos princípios do Programa.
+- A criação e apresentação prévia da programação, cronograma e periodicidade de publicação dos materiais.
+- A avaliação do alcance e impacto das ações e organização de novas estratégias de publicação devidamente alinhadas com a equipe técnica e coordenação do Programa."));
+
 
    $pdf->Ln();
    $pdf->Ln();
