9d3afaaefdf86f302f776336366483a383c46fc5
LoreleiGab <loreleigab@hotmail.com>
2020-06-18 10:58:26 -0300

    Relatório modelo email para empresas com o email do usuário logado

--- pdf/rlt_email_empresas.php
+++ pdf/rlt_email_empresas.php
@@ -10,8 +10,8 @@ $conexao = bancoMysqli();
 
 //CONSULTA
 $id_ped=$_GET['id'];
-$idUsuario = $_GET['idUsuario'];
 $modeloEmail = $_GET['modelo'];
+$idUsuario = $_GET['idUsuario'];
 
 switch ($modeloEmail) {
     case 'empresas':
@@ -29,7 +29,7 @@ $pedido = siscontrat($id_ped);
 $pj = siscontratDocs($pedido['IdProponente'],2);
 $rep01 = siscontratDocs($pj['Representante01'],3);
 $pj = siscontratDocs($pedido['IdProponente'],2);
-$nomeUsuario = recuperaDados('ig_usuario',$idUsuario,'idUsuario')['nomeCompleto'];
+$usuario = $conexao->query("SELECT nomeCompleto,email FROM ig_usuario WHERE idUsuario = '$idUsuario'")->fetch_assoc();
 
 dataPagamento($id_ped);
 
@@ -60,19 +60,21 @@ header("Content-Disposition: attachment;Filename=$dataAtual - Processo SEI $Nume
 <meta http-equiv=\"Content-Type\" content=\"text/html; charset=Windows-1252\">
 <body>
 
-<p align="justify">Prezado(a) Senhor(a) <?= $rep01Nome?>,</p>
-<p align="justify">Tendo em vista a apresentação <?= $Objeto?>, na data/período de <?= $Periodo?>, encaminho em anexo, para fins de pagamento, os itens abaixo relacionados:</p>
-<p align="justify">a) Recibo da nota de empenho (para ser assinado pelo(a) representante legal da Empresa);</p>
-<p align="justify">b) Pedido de pagamento (para ser assinado pelo(a) representante legal);</p>
-<p align="justify">c)Instruções para Emissão da Nota Fiscal Eletrônica;</p>
-<p align="justify">d) <?=$item4?></p>
-<p align="justify">Para fins de arquivamento da empresa, segue também o Anexo e a Nota de Empenho da referida contratação.  </p>
-<p align="justify">Informo que a documentação acima citada deverá ser devolvida digitalizada, <strong>somente através do e-mail smc.pagamentosartisticos@gmail.com, em até 48 horas, impreterivelmente.</strong></p>
+<p style="text-align:justify">Prezado(a) Senhor(a) <?= $rep01Nome?>,</p>
 <p>&nbsp;</p>
-<p align="justify">Atenciosamente,</p>
-<p><?=$nomeUsuario?></p>
-<p>SMC / Pagamentos Artísticos</p>
-<p>Tel: (11) 3397-0191</p>
+<p style="text-align:justify">Tendo em vista a apresentação <?= $Objeto?>, na data/período de <?= $Periodo?>, encaminho em anexo, para fins de pagamento, os itens abaixo relacionados:</p>
+<p style="text-align:justify">a) Recibo da nota de empenho (para ser assinado pelo(a) representante legal da Empresa);</p>
+<p style="text-align:justify">b) Pedido de pagamento (para ser assinado pelo(a) representante legal);</p>
+<p style="text-align:justify">c) Instruções para Emissão da Nota Fiscal Eletrônica;</p>
+<p style="text-align:justify">d) <?=$item4?></p>
+<p style="text-align:justify">Para fins de arquivamento da empresa, segue também o Anexo e a Nota de Empenho da referida contratação.  </p>
+<p style="text-align:justify">Informo que a documentação acima citada deverá ser devolvida digitalizada, <strong>somente através do e-mail <?= $usuario['email'] ?>, em até 48 horas, impreterivelmente.</strong></p>
+<p>&nbsp;</p>
+<p style="text-align:justify">Atenciosamente,</p>
+<p>&nbsp;</p>
+<p><?=$usuario['nomeCompleto']?><br>
+SMC / Pagamentos Artísticos</p>
+<!--<p>Tel: (11) 3397-0191</p>-->
 <p>&nbsp;</p>
 </body>
 </html>
