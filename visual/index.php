1e1766d45be072f3f899b2897d890fd031b2aa95
Lorelei <loreleigab@hotmail.com>
2020-11-13 22:44:07 -0300

    Remoção do alerta de sistema fora do ar

--- visual/index.php
+++ visual/index.php
@@ -18,12 +18,12 @@ require "cabecalho.php";
 if(isset($_GET['perfil'])){ 
 	include "../perfil/".$_GET['perfil'].".php";
 
-if(!isset($_SESSION['alert'])){
-	$_SESSION['alert'] = "ok";
-?>
-	<script>alert("ATENÇÃO! O sistema entrará em manutenção das 21h10 às 23h59 no dia de hoje.")</script>
-<?php
-}
+    /*if(!isset($_SESSION['alert'])){
+        $_SESSION['alert'] = "ok";
+    */?><!--
+        <script>alert("ATENÇÃO! O sistema entrará em manutenção das 21h10 às 23h59 no dia de hoje.")</script>
+    --><?php
+/*    }*/
 
 }else{
 	include "../perfil/inicio.php";
